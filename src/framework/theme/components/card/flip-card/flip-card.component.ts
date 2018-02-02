f3ac3a420d2807c1c355dbcb664b2c19c33feef1
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2018-02-02 15:25:35 +0300

    fix(docs): use correct tag in usage example

--- src/framework/theme/components/card/flip-card/flip-card.component.ts
+++ src/framework/theme/components/card/flip-card/flip-card.component.ts
@@ -8,14 +8,14 @@ import { Component, Input, HostBinding } from '@angular/core';
  * @example
  *
  * ```
- * <nb-reveal-card>
+ * <nb-flip-card>
  *   <nb-card-front>
  *     <nb-card><nb-card-body>Front Card</nb-card-body></nb-card>
  *   </nb-card-front>
  *   <nb-card-back>
  *     <nb-card><nb-card-body>Back Card</nb-card-body></nb-card>
  *   </nb-card-back>
- * </nb-reveal-card>
+ * </nb-flip-card>
  * ```
  */
 @Component({
