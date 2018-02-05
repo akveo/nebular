86fe788fb0a358c8d4d41c4911d3f394b5882855
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-02-05 23:54:55 +0300

    fix(popover): adjustment helper docs

--- src/framework/theme/components/popover/adjustment.helper.ts
+++ src/framework/theme/components/popover/adjustment.helper.ts
@@ -40,6 +40,13 @@ export class NbAdjustmentHelper {
   /**
    * Calculated {@link NbPosition} based on placed element, host element,
    * placed element placement and adjustment strategy.
+   *
+   * @param placed {ClientRect} placed element relatively host.
+   * @param host {ClientRect} host element.
+   * @param placement {NbPlacement} placed element placement relatively host.
+   * @param adjustment {NbAdjustment} adjustment strategy.
+   *
+   * @return {NbPosition} calculated position.
    * */
   static adjust(placed: ClientRect, host: ClientRect, placement: NbPlacement, adjustment: NbAdjustment): NbPosition {
     const placements = NB_ORDERED_PLACEMENTS[adjustment].slice();
@@ -53,7 +60,12 @@ export class NbAdjustmentHelper {
   }
 
   /**
-   * Searches first adjustment which doesn't go beyond.
+   * Searches first adjustment which doesn't go beyond the viewport.
+   *
+   * @param placed {ClientRect} placed element relatively host.
+   * @param possible {NbPosition[]} possible positions list ordered according to adjustment strategy.
+   *
+   * @return {NbPosition} calculated position.
    * */
   private static chooseBest(placed: ClientRect, possible: NbPosition[]): NbPosition {
     return possible.find(adjust => NbAdjustmentHelper.inViewPort(placed, adjust)) || possible.shift();
@@ -61,16 +73,39 @@ export class NbAdjustmentHelper {
 
   /**
    * Finds out is adjustment doesn't go beyond of the view port.
+   *
+   * @param placed {ClientRect} placed element relatively host.
+   * @param position {NbPosition} position of the placed element.
+   *
+   * @return {boolean} true if placed element completely viewport.
    * */
-  private static inViewPort(placed: ClientRect, adjustment: NbPosition): boolean {
-    return adjustment.position.top - window.pageYOffset > 0 &&
-      adjustment.position.left - window.pageXOffset > 0 &&
-      adjustment.position.top + placed.height < window.innerHeight + window.pageYOffset &&
-      adjustment.position.left + placed.width < window.innerWidth + window.pageXOffset;
+  private static inViewPort(placed: ClientRect, position: NbPosition): boolean {
+    return position.position.top - window.pageYOffset > 0 &&
+      position.position.left - window.pageXOffset > 0 &&
+      position.position.top + placed.height < window.innerHeight + window.pageYOffset &&
+      position.position.left + placed.width < window.innerWidth + window.pageXOffset;
   }
 
   /**
-   * Loop placements list to make this.placement start point.
+   * Reorder placements list to make placement start point and fit {@link NbAdjustment}
+   *
+   * @param placement {NbPlacement} active placement
+   * @param placements {NbPlacement[]} placements list according to the active adjustment strategy.
+   *
+   * @return {NbPlacement[]} correctly ordered placements list.
+   *
+   * @example order placements for {@link NbPlacement#RIGHT} and {@link NbAdjustment#CLOCKWISE}
+   * ```
+   * const placements = NB_ORDERED_PLACEMENTS[NbAdjustment.CLOCKWISE];
+   * const ordered = orderPlacement(NbPlacement.RIGHT, placements);
+   *
+   * expect(ordered).toEqual([
+   *  NbPlacement.RIGHT,
+   *  NbPlacement.BOTTOM,
+   *  NbPlacement.LEFT,
+   *  NbPlacement.TOP,
+   * ]);
+   * ```
    * */
   private static orderPlacements(placement: NbPlacement, placements: NbPlacement[]): NbPlacement[] {
     const index = placements.indexOf(placement);
