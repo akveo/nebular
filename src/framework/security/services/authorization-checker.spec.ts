e7c98ca47dea5618712b1c40f1f06eb7ce0ca99b
Dmitry Nehaychik <4dmitr@gmail.com>
2018-02-07 17:15:57 +0300

    wip(acl): documentation

--- src/framework/security/services/authorization-checker.spec.ts
+++ src/framework/security/services/authorization-checker.spec.ts
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
 import { async, TestBed, inject } from '@angular/core/testing';
 import { of as observableOf } from 'rxjs/observable/of';
 
