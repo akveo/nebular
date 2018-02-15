6de9049c05a5811f992c3efa41f5af7fddd105ff
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2018-02-15 19:13:03 +0300

    fix(auth): add missing auth result

--- src/framework/auth/services/index.ts
+++ src/framework/auth/services/index.ts
@@ -2,3 +2,4 @@ export * from './auth.service';
 export * from './token.service';
 export * from './interceptors/jwt-interceptor';
 export * from './interceptors/simple-interceptor';
+export * from './auth-result';
