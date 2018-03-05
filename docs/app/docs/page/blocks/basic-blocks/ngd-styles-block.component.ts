ac2e1bb2d0e8f2f9431f5f020a6a109ea4b77e5e
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-03-05 22:50:47 +0300

    feat(docs): remove styles block sub header

--- docs/app/docs/page/blocks/basic-blocks/ngd-styles-block.component.ts
+++ docs/app/docs/page/blocks/basic-blocks/ngd-styles-block.component.ts
@@ -11,9 +11,6 @@ import { DocsService } from '../../../docs.service';
   selector: 'ngd-styles-block',
   template: `
     <div class="block-container" *ngFor="let style of classStyles">
-      <p class="block-title"><a [routerLink]="" fragment="{{className}}Styles" ngdFragment></a>
-        Component themable styles
-      </p>
       <div class="table-container">
         <table class="table table-striped">
           <thead>
