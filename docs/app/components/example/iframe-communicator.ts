372e6a9524393e8158528b3ab85714fd17428e20
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-03-28 00:18:10 +0300

    fix(docs): use global objects injection

--- docs/app/components/example/iframe-communicator.ts
+++ docs/app/components/example/iframe-communicator.ts
@@ -3,16 +3,20 @@ import { Observable } from 'rxjs/Observable';
 import { fromEvent as observableFromEvent } from 'rxjs/observable/fromEvent';
 import { filter } from 'rxjs/operators/filter';
 import { map } from 'rxjs/operators/map';
+import { NbWindow } from '@nebular/theme';
 
 @Injectable()
 export class IframeCommunicatorService {
 
-  public send(payload: any, target: Window = window.parent) {
+  constructor(private window: NbWindow) {
+  }
+
+  public send(payload: any, target: Window = this.window.parent) {
     target.postMessage(payload, '*');
   }
 
   public receive(id: string): Observable<any> {
-    return observableFromEvent(window, 'message')
+    return observableFromEvent(this.window, 'message')
       .pipe(
         filter((msg: any) => msg.data && msg.data.id === id),
         map((msg: any) => msg.data),
