1186ac6f6fcc9646c89478552574ce09d3cdd5c0
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2018-04-05 18:36:54 +0300

    docs(direction-service): add service documentation

--- src/framework/theme/services/direction.service.ts
+++ src/framework/theme/services/direction.service.ts
@@ -1,13 +1,22 @@
 import { InjectionToken, Optional, Inject, Injectable } from '@angular/core';
 import { NbDocument } from '../theme.options';
 
+/**
+ * Layout direction.
+ * */
 export enum NbLayoutDirection {
   LTR = 'ltr',
   RTL = 'rtl',
 };
 
+/**
+ * Layout direction setting injection token.
+ * */
 export const NB_LAYOUT_DIRECTION = new InjectionToken<NbLayoutDirection>('Layout direction');
 
+/**
+ * Layout Direction Service. Allows you to listen to menu events, or to interact with a menu.
+ */
 @Injectable()
 export class NbLayoutDirectionService {
   constructor(
@@ -18,18 +27,34 @@ export class NbLayoutDirectionService {
     this.setDirection(<NbLayoutDirection>dir);
   }
 
+  /**
+   * Returns true if layout direction set to left to right.
+   * @returns boolean.
+   * */
   public get isLtr() {
     return this.dir === NbLayoutDirection.LTR;
   }
 
+  /**
+   * Returns true if layout direction set to right to left.
+   * @returns boolean.
+   * */
   public get isRtl() {
     return this.dir === NbLayoutDirection.RTL;
   }
 
+  /**
+   * Returns current layout direction.
+   * @returns NbLayoutDirection.
+   * */
   getDirection(): NbLayoutDirection {
     return this.dir;
   }
 
+  /**
+   * Sets layout direction.
+   * @param dir {NbLayoutDirection} direction to set.
+   * */
   setDirection(dir: NbLayoutDirection) {
     this.dir = dir;
     this.document.dir = this.dir;
