e562a37af7c3433aedb5ddb7f01107259095d59e
Nikita Poltoratsky <parapupum@gmail.com>
2018-04-21 17:41:42 +0300

    feat(docs): add theme switcher

--- docs/app/components/example/example-renderer.component.ts
+++ docs/app/components/example/example-renderer.component.ts
@@ -1,26 +1,48 @@
-import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { IframeCommunicatorService } from './iframe-communicator';
 import { Router } from '@angular/router';
-import { NB_DOCUMENT } from '@nebular/theme';
+import { NB_DOCUMENT, NbThemeService } from '@nebular/theme';
+import { takeWhile } from 'rxjs/operators/takeWhile';
 
 @Component({
   selector: 'ngd-example-renderer',
   template: '<router-outlet></router-outlet>',
 })
-export class NgdExampleRendererComponent implements OnInit, AfterViewInit {
+export class NgdExampleRendererComponent implements OnInit, AfterViewInit, OnDestroy {
   private id: string;
+  private alive: boolean = true;
 
   constructor(private communicator: IframeCommunicatorService,
+              private themeService: NbThemeService,
               private router: Router,
               @Inject(NB_DOCUMENT) private document) {
   }
 
   ngOnInit() {
-    this.id = this.getId();
+    this.setupId();
+    this.subscribeOnThemeSwitch();
   }
 
   ngAfterViewInit() {
     this.sendHeight();
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+  }
+
+  private setupId() {
+    this.id = this.getId();
+  }
+
+  private subscribeOnThemeSwitch() {
+    this.communicator.receive(this.id)
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(payload => this.changeTheme(payload))
+  }
+
+  private changeTheme(payload) {
+    this.themeService.changeTheme(payload.theme)
   }
 
   private getId(): string {
