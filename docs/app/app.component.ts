822b50b313890b81c573601452b04afc4fadc778
nixa <4dmitr@gmail.com>
2018-04-24 22:56:52 +0300

    feat(docs): docs page (WIP)

--- docs/app/app.component.ts
+++ docs/app/app.component.ts
@@ -4,14 +4,7 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, Inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-
-import { Subscription } from 'rxjs';
-import { delay, merge } from 'rxjs/operators';
-import { DocsService } from './docs/docs.service';
-import { Analytics } from './docs/utils/analytics.service';
-import { NB_WINDOW } from '@nebular/theme';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngd-app-root',
@@ -19,50 +12,48 @@ import { NB_WINDOW } from '@nebular/theme';
     <router-outlet></router-outlet>
   `,
 })
-export class NgdAppComponent implements AfterViewInit, OnDestroy, OnInit {
+export class NgdAppComponent {
 
-  private fragmentSubscription: Subscription;
+  // private fragmentSubscription: Subscription;
+  //
+  //
+  // constructor(@Inject(NB_WINDOW) private window,
+  //             private docsService: DocsService,
+  //             private router: Router,
+  //             private route: ActivatedRoute,
+  //             private elementRef: ElementRef,
+  //             private analytics: Analytics) {
+  // }
+  //
+  // ngOnInit(): void {
+  //   this.analytics.trackPageViews();
+  // }
 
+  // // TODO: refactor this
+  // ngAfterViewInit() {
+  //   // this.fragmentSubscription = this.route.fragment
+  //   //   .merge(this.docsService.onFragmentClick())
+  //   //   .delay(1)
+  //   //   .subscribe(fr => this.processFragment(fr));
+  // }
 
-  constructor(@Inject(NB_WINDOW) private window,
-              private docsService: DocsService,
-              private router: Router,
-              private route: ActivatedRoute,
-              private elementRef: ElementRef,
-              private analytics: Analytics) {
-  }
+  // processFragment(fr) {
+  //   if (fr) {
+  //     const el = this.elementRef.nativeElement.querySelector(`#${fr}`);
+  //     if (el) {
+  //       el.scrollIntoView();
+  //       if (new RegExp(/themes/i).test(this.router.url)) {
+  //         this.window.scrollBy(0, -235); // header + theme-header
+  //       } else {
+  //         this.window.scrollBy(0, -80);
+  //       }
+  //     }
+  //   } else {
+  //     this.window.scrollTo(0, 0);
+  //   }
+  // }
 
-  ngOnInit(): void {
-    this.analytics.trackPageViews();
-  }
-
-  // TODO: refactor this
-  ngAfterViewInit() {
-    this.fragmentSubscription = this.route.fragment
-      .pipe(
-        merge(this.docsService.onFragmentClick()),
-        delay(1),
-      )
-      .subscribe(fr => this.processFragment(fr));
-  }
-
-  processFragment(fr) {
-    if (fr) {
-      const el = this.elementRef.nativeElement.querySelector(`#${fr}`);
-      if (el) {
-        el.scrollIntoView();
-        if (new RegExp(/themes/i).test(this.router.url)) {
-          this.window.scrollBy(0, -235); // header + theme-header
-        } else {
-          this.window.scrollBy(0, -80);
-        }
-      }
-    } else {
-      this.window.scrollTo(0, 0);
-    }
-  }
-
-  ngOnDestroy() {
-    this.fragmentSubscription.unsubscribe();
-  }
+  // ngOnDestroy() {
+  //   // this.fragmentSubscription.unsubscribe();
+  // }
 }
--- docs/app/app.module.ts
+++ docs/app/app.module.ts
@@ -19,39 +19,8 @@ import {
 } from '@nebular/theme';
 import { NgdAppComponent } from './app.component';
 import { routes } from './app.routes';
-import { NgdHomepageComponent } from './homepage/homepage.component';
-import { DocsService } from './docs/docs.service';
-import { NgdDocsComponent } from './docs/docs.component';
-import { NgdPageComponent } from './docs/page/page.component';
-import { NgdMarkdownComponent } from './docs/page/blocks/markdown/ngd-markdown-block.component';
-import { NgdPropsBlockComponent } from './docs/page/blocks/basic-blocks/ngd-props-block.component';
-import { NgdMethodsBlockComponent } from './docs/page/blocks/basic-blocks/ngd-methods-block.component';
-import { NgdDescriptionDirective } from './docs/utils/ngd-description.directive';
-import { NgdBlockComponent } from './docs/page/blocks/ngd-block.component';
-import { NgdTabbedBlockComponent } from './docs/page/blocks/tabbed-block/ngd-tabbed-block.component';
-import { NgdLiveExampleComponent } from './docs/page/blocks/live-example/ngd-live-example.component';
 
-import { NgdHeaderComponent } from './components/header/ngd-header.component';
-import { NgdFooterComponent } from './components/footer/ngd-footer.component';
-import { NgdStylesBlockComponent } from './docs/page/blocks/basic-blocks/ngd-styles-block.component';
-import { NgdOverviewBlockComponent } from './docs/page/blocks/basic-blocks/ngd-overview-block.component';
-import { NgdComponentBlockComponent } from './docs/page/blocks/ngd-component-block.component';
-import { NgdFragmentDirective } from './docs/utils/ngd-fragment.directive';
-import { NgdThemeComponent } from './docs/page/blocks/ngd-theme-block.component';
-import { NgdSassPropValueDirective } from './docs/utils/ngd-color-swatch.directive';
-import { SwiperModule } from 'angular2-useful-swiper';
-import { Analytics } from './docs/utils/analytics.service';
-import { BlockHelperService } from './docs/utils/block-helper.service';
 import { NgdExampleRendererComponent } from './components/example/example-renderer.component';
-import { IframeCommunicatorService } from './components/example/iframe-communicator';
-import { NgdCodeBlockComponent } from './docs/page/blocks/basic-blocks/ngd-code-block.component';
-import { NgdInlineExampleComponent } from './docs/page/blocks/inline-example/ngd-inline-example.component';
-import { NgdTabbedExampleComponent } from './docs/page/blocks/inline-example/ngd-tabbed-example.component';
-import { NgdExampleComponent } from './docs/page/blocks/inline-example/ngd-example.component';
-import { CodeLoaderService } from './docs/utils/code-loader.service';
-import { NgdMarkdownFileComponent } from './docs/page/blocks/markdown/ngd-markdown-file.component';
-import { NgdHighlightService } from './docs/utils/highlight.service';
-import { NgdStackedExampleComponent } from './docs/page/blocks/stacked-example/stacked-examples.component';
 
 @NgModule({
   imports: [
@@ -61,50 +30,19 @@ import { NgdStackedExampleComponent } from './docs/page/blocks/stacked-example/s
     NbThemeModule,
     NbSidebarModule,
     NbCardModule,
-    SwiperModule,
     NbLayoutModule,
     NbTabsetModule,
     NbCheckboxModule,
     NbMenuModule.forRoot(),
-    NbThemeModule.forRoot({ name: 'docs' }),
+    NbThemeModule.forRoot({ name: '' }),
     NbSidebarModule.forRoot(),
     RouterModule.forRoot(routes, { useHash: true }),
   ],
   declarations: [
     NgdAppComponent,
-    NgdHomepageComponent,
-    NgdDocsComponent,
-    NgdPageComponent,
-    NgdMarkdownComponent,
-    NgdPropsBlockComponent,
-    NgdMethodsBlockComponent,
-    NgdDescriptionDirective,
-    NgdHeaderComponent,
-    NgdFooterComponent,
-    NgdStylesBlockComponent,
-    NgdComponentBlockComponent,
-    NgdFragmentDirective,
-    NgdThemeComponent,
-    NgdBlockComponent,
-    NgdTabbedBlockComponent,
-    NgdOverviewBlockComponent,
-    NgdLiveExampleComponent,
-    NgdSassPropValueDirective,
     NgdExampleRendererComponent,
-    NgdInlineExampleComponent,
-    NgdCodeBlockComponent,
-    NgdTabbedExampleComponent,
-    NgdExampleComponent,
-    NgdStackedExampleComponent,
-    NgdMarkdownFileComponent,
   ],
   providers: [
-    IframeCommunicatorService,
-    NgdHighlightService,
-    CodeLoaderService,
-    BlockHelperService,
-    DocsService,
-    Analytics,
     Title,
   ],
   entryComponents: [
