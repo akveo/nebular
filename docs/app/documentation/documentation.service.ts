3e2b55d6d7e3ec24a79f3cc07757e9db26f5f58d
nixa <4dmitr@gmail.com>
2018-04-28 11:23:12 +0300

    refactor(docs): prepare menu refactoring

--- docs/app/documentation/documentation.service.ts
+++ docs/app/documentation/documentation.service.ts
@@ -5,64 +5,28 @@
  */
 
 import { Injectable } from '@angular/core';
-
-import { Subject } from 'rxjs/Subject';
-import { Observable } from 'rxjs/Observable';
+import { NbMenuItem } from '@nebular/theme';
 
 import { STRUCTURE } from '../../structure';
-
 const PARSED_DOCS: any = require('../../output.json');
 
 @Injectable()
 export class NgdDocumentationService {
 
-  private fragments$ = new Subject<string>();
-  private preparedStructure = this.prepareStructure(this.getStructure(), this.getParsedDocs());
-
   getStructure(): any {
     return STRUCTURE;
-  }
-
-  getPreparedMenu(prependMenu?: any): any {
-    return this.prepareMenu(this.getStructure(), '/docs', prependMenu);
   }
 
   getParsedDocs(): any {
     return PARSED_DOCS;
   }
 
-  getPreparedStructure(): any {
-    return this.preparedStructure;
-  }
-
-  emitFragment(fragment: string): void {
-    this.fragments$.next(fragment);
-  }
-
-  onFragmentClick(): Observable<string> {
-    return this.fragments$.share();
-  }
-
-  mapThemedValues(classStyles: any): any {
-    return classStyles.map(item => {
-      item.styles.map(prop => {
-        prop.themedValues = [];
-        for (const themeName in PARSED_DOCS.themes) {
-          if (PARSED_DOCS.themes.hasOwnProperty(themeName)) {
-            prop.themedValues.push({
-              theme: PARSED_DOCS.themes[themeName].name,
-              value: PARSED_DOCS.themes[themeName].data[prop.name].value,
-            });
-          }
-        }
-        return prop;
-      });
-      return item;
-    })
+  getPreparedMenu(basePath: string): any {
+    return this.prepareMenu(this.getStructure(), { link: basePath });
   }
 
   protected prepareStructure(structure: any, preparedDocs: any): any {
-    structure.map((item: any) => {
+    return structure.map((item: any) => {
       if (item.type === 'block' && typeof item.source === 'string') {
         if (item.block === 'theme') {
           item.source = preparedDocs.themes[item.source];
@@ -80,42 +44,34 @@ export class NgdDocumentationService {
       if (item.children) {
         item.children = this.prepareStructure(item.children, preparedDocs);
       }
+
+      return item;
     });
-    return structure;
   }
 
-  protected prepareMenu(structure, parentLink?: string, prependMenu?: any, parentItem?: any): any {
+  prepareMenu(structure, parent = null) {
+    return structure
+      .filter(item => item.name && item.type !== 'block')
+      .map((item: any) => {
+        const menuItem: NbMenuItem = {
+          title: item.name,
+          link: this.createItemLink(item),
+          pathMatch: 'prefix',
+          parent,
+          data: item,
+        };
 
-    let menuItems = structure.map((item: any) => {
-      if (item.name) {
-        const menuItem: any = {};
-        const name = item.name.replace(/[^a-zA-Z0-9\s]+/g, '').replace(/\s/g, '-').toLowerCase();
-        const itemLink = item.type === 'block' ? parentLink : `${parentLink ? parentLink : ''}/${name}`;
-
-        if (item.type !== 'section' || !item.isSubpages) {
-          menuItem['link'] = itemLink;
-        }
-        (item.type === 'block') ? menuItem['data'] = parentItem : menuItem['data'] = item;
-        if (item.type === 'block') {
-          menuItem['fragment'] = item.name;
-        }
-        menuItem['pathMatch'] = 'prefix';
-        menuItem['title'] = item.name;
-
+        // TODO: not very good check
         if (item.children && item.children[0] && item.children[0].type === 'page') {
-          menuItem['children'] = this.prepareMenu(item.children, itemLink);
+          menuItem.children = this.prepareMenu(item.children, item);
         }
-        if (item.children && item.type === 'page' && item.isSubpages) {
-          menuItem['children'] = this.prepareMenu(item.children, itemLink, null, item);
-        }
+
         return menuItem;
-      }
-    });
+      });
+  }
 
-    if (prependMenu) {
-      menuItems = prependMenu.concat(menuItems);
-    }
-
-    return menuItems;
+  protected createItemLink(item: any) {
+    const url = item.name.replace(/[^a-zA-Z0-9\s]+/g, '').replace(/\s/g, '-').toLowerCase();
+    return item.parent ? `${item.parent.link}/${url}` : url;
   }
 }
