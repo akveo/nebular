3c26e41ecf62c7c8455089488b44247174ad74bb
nixa <4dmitr@gmail.com>
2018-05-07 23:40:15 +0300

    feat(docs): fragment scroll fix

--- docs/app/@theme/components/fragment-target/fragment-target.directive.ts
+++ docs/app/@theme/components/fragment-target/fragment-target.directive.ts
@@ -1,5 +1,6 @@
-import { Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import {Directive, ElementRef, Inject, Input, OnDestroy, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NB_WINDOW } from '@nebular/theme';
 import { takeWhile } from 'rxjs/operators/takeWhile';
 
 @Directive({
@@ -12,10 +13,12 @@ export class NgdFragmentTargetDirective implements OnInit, OnDestroy {
   @Input() name: string;
 
   private alive = true;
+  private readonly marginFromTop = 120;
 
   // I initialize the fragment-target directive.
   constructor(
     private activatedRoute: ActivatedRoute,
+    @Inject(NB_WINDOW) private window,
     private elementRef: ElementRef) {
   }
 
@@ -24,11 +27,7 @@ export class NgdFragmentTargetDirective implements OnInit, OnDestroy {
       .pipe(takeWhile(() => this.alive))
       .subscribe((fragment: string) => {
         if (fragment && [this.id, this.name].includes(fragment)) {
-
-          this.elementRef.nativeElement.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-          });
+          this.window.scrollTo(0, this.elementRef.nativeElement.offsetTop - this.marginFromTop);
         }
       });
   }
