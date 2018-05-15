7109a69fe084cbf961d0cbe0366a962ef753348b
Dmitry Nehaychik <4dmitr@gmail.com>
2018-05-15 19:18:35 +0300

    refactor(live-example): add themes to example, simplify styles

--- docs/app/blocks/components/live-example-block/live-example-block.component.ts
+++ docs/app/blocks/components/live-example-block/live-example-block.component.ts
@@ -7,6 +7,7 @@ import {
   ViewChild,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
+  HostBinding,
 } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import {takeWhile} from 'rxjs/operators/takeWhile';
@@ -15,26 +16,7 @@ import { NgdIframeCommunicatorService } from '../../../@theme/services';
 @Component({
   selector: 'ngd-live-example-block',
   styleUrls: ['./live-example-block.component.scss'],
-  template: `
-    <section class="live-example-header">
-      <div class="block-title">{{title}}</div>
-      <div class="example-block-actions">
-        <button type="button" class="btn action-item action-button">
-          <i class="icon feather-code"></i>
-        </button>
-        <button type="button" class="btn action-item action-button">
-          <i class="icon feather-external-link"></i>
-        </button>
-        <div class="action-selector">
-          <i class="icon feather-aperture"></i>
-          <select class="action-item" [(ngModel)]="defaultTheme"  (change)="switchTheme($event.target.value)">
-            <option *ngFor="let theme of themes" [value]="theme.value">{{theme.label}}</option>
-          </select>
-        </div>
-      </div>
-    </section>
-    <iframe #iframe *ngIf="id" [src]="url" [style.height.px]="iframeHeight"></iframe>
-  `,
+  templateUrl: './live-example-block.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NgdLiveExampleBlockComponent implements OnInit, OnDestroy {
@@ -43,8 +25,17 @@ export class NgdLiveExampleBlockComponent implements OnInit, OnDestroy {
   @Input() id: string;
   @Input() title: string;
 
-  iframeHeight: number;
+  @HostBinding('class.theme-default')
+  private get isDefault() {
+    return this.currentTheme === 'default';
+  }
 
+  @HostBinding('class.theme-cosmic')
+  private get isCosmic() {
+    return this.currentTheme === 'cosmic';
+  }
+
+  iframeHeight: number;
   alive: boolean = true;
 
   themes: {label: string; value: string}[] = [
@@ -53,6 +44,7 @@ export class NgdLiveExampleBlockComponent implements OnInit, OnDestroy {
   ];
 
   defaultTheme: string = 'default';
+  currentTheme: string = this.defaultTheme;
 
   get url(): SafeUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(`#/example/${this.id}`);
@@ -82,6 +74,7 @@ export class NgdLiveExampleBlockComponent implements OnInit, OnDestroy {
   }
 
   switchTheme(theme: string) {
+    this.currentTheme = theme;
     this.communicator.send({ id: this.id, theme }, this.iframeWindow);
   }
 }
