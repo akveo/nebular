2c45c7d4647bf1fc012d1540ac3c225a6d0099b0
Dmitry Nehaychik <4dmitr@gmail.com>
2018-05-15 18:20:08 +0300

    refactor(docs): place overview content on a tab

--- docs/app/blocks/components/overview-block/overview-block.component.ts
+++ docs/app/blocks/components/overview-block/overview-block.component.ts
@@ -3,15 +3,22 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'ngd-overview-block',
   template: `
-    <ng-container class="description" *ngFor="let node of overview">
-      <ngd-md-block *ngIf="node.type === 'text'" [source]="node.content" [title]="source.name"></ngd-md-block>
-      <ngd-live-example-block *ngIf="node.type === 'live-example'" [id]="node.content" [title]="'example'">
-      </ngd-live-example-block>
-      <ngd-inline-example-block *ngIf="node.type === 'inline-example'" [content]="node.content">
-      </ngd-inline-example-block>
-      <ngd-stacked-example-block *ngIf="node.type === 'example'" [content]="node.content">
-      </ngd-stacked-example-block>
-    </ng-container>
+    <nb-card>
+      <a [name]="source.slag"></a>
+      <nb-card-body>
+        <ng-container class="description" *ngFor="let node of overview">
+          <ng-container *ngIf="node.type === 'text'">
+            <div *ngFor="let section of node.content" [innerHtml]="section.html"></div>
+          </ng-container>
+          <ngd-live-example-block *ngIf="node.type === 'live-example'" [id]="node.content" [title]="'example'">
+          </ngd-live-example-block>
+          <ngd-inline-example-block *ngIf="node.type === 'inline-example'" [content]="node.content">
+          </ngd-inline-example-block>
+          <ngd-stacked-example-block *ngIf="node.type === 'example'" [content]="node.content">
+          </ngd-stacked-example-block>
+        </ng-container>
+      </nb-card-body>
+    </nb-card>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
