56b695ad23c6383e0fff812a097c85891552d499
Denis Strigo <denis2295@gmail.com>
2018-05-15 14:19:33 +0300

    feat(docs): add css style for live example component, update feather fonts (#422)

--- docs/app/blocks/components/live-example-block/live-example-block.component.ts
+++ docs/app/blocks/components/live-example-block/live-example-block.component.ts
@@ -1,4 +1,13 @@
-import { Component, ElementRef, Input, OnInit, OnDestroy, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import {takeWhile} from 'rxjs/operators/takeWhile';
 import { NgdIframeCommunicatorService } from '../../../@theme/services';
@@ -7,7 +16,23 @@ import { NgdIframeCommunicatorService } from '../../../@theme/services';
   selector: 'ngd-live-example-block',
   styleUrls: ['./live-example-block.component.scss'],
   template: `
-    <p><nb-checkbox [(ngModel)]="isCosmic" (change)="switchTheme()">Switch theme</nb-checkbox></p>
+    <section class="live-example-header">
+      <div class="block-title">{{title}}</div>
+      <div class="example-block-actions">
+        <button type="button" class="btn action-item action-button">
+          <i class="icon feather-code"></i>
+        </button>
+        <button type="button" class="btn action-item action-button">
+          <i class="icon feather-external-link"></i>
+        </button>
+        <div class="action-selector">
+          <i class="icon feather-aperture"></i>
+          <select class="action-item" [(ngModel)]="defaultTheme"  (change)="switchTheme($event.target.value)">
+            <option *ngFor="let theme of themes" [value]="theme.value">{{theme.label}}</option>
+          </select>
+        </div>
+      </div>
+    </section>
     <iframe #iframe *ngIf="id" [src]="url" [style.height.px]="iframeHeight"></iframe>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,10 +41,18 @@ export class NgdLiveExampleBlockComponent implements OnInit, OnDestroy {
 
   @ViewChild('iframe') iframe: ElementRef;
   @Input() id: string;
+  @Input() title: string;
 
   iframeHeight: number;
-  isCosmic: boolean;
+
   alive: boolean = true;
+
+  themes: {label: string; value: string}[] = [
+    { label: 'Default', value: 'default' },
+    { label: 'Cosmic', value: 'cosmic' },
+  ];
+
+  defaultTheme: string = 'default';
 
   get url(): SafeUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(`#/example/${this.id}`);
@@ -29,22 +62,26 @@ export class NgdLiveExampleBlockComponent implements OnInit, OnDestroy {
     return this.iframe.nativeElement.contentWindow;
   }
 
-  constructor(private sanitizer: DomSanitizer,
-              private communicator: NgdIframeCommunicatorService) {
+  constructor(private changeDetection: ChangeDetectorRef,
+              private sanitizer: DomSanitizer,
+              private communicator: NgdIframeCommunicatorService,
+  ) {
   }
 
   ngOnInit(): void {
     this.communicator.receive(this.id)
       .pipe(takeWhile(() => this.alive))
-      .subscribe(it => this.iframeHeight = it.height);
+      .subscribe(it => {
+        this.iframeHeight = it.height;
+        this.changeDetection.detectChanges();
+      });
   }
 
   ngOnDestroy(): void {
     this.alive = false;
   }
 
-  switchTheme() {
-    const theme = this.isCosmic ? 'cosmic' : 'default';
+  switchTheme(theme: string) {
     this.communicator.send({ id: this.id, theme }, this.iframeWindow);
   }
 }
