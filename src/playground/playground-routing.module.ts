25008d52869f371b8c4d39845c16b81a6ef5c2a6
nixa <4dmitr@gmail.com>
2018-05-22 00:15:39 +0300

    feat(docs): not found example

--- src/playground/playground-routing.module.ts
+++ src/playground/playground-routing.module.ts
@@ -67,7 +67,7 @@ export const routes: Routes = [
         component: NbCheckboxStatusComponent,
       },
       {
-        path: 'checkbox-disabled/checkbox-disabled.component',
+        path: 'checkbox/checkbox-disabled.component',
         component: NbCheckboxDisabledComponent,
       },
       {
