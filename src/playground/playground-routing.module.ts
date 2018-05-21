b4124376fbb738ab4a5dbbd6e4a8ca83bd0e133b
Dmitry Nehaychik <4dmitr@gmail.com>
2018-05-21 18:29:11 +0300

    refactor(playground): layout examples

--- src/playground/playground-routing.module.ts
+++ src/playground/playground-routing.module.ts
@@ -7,7 +7,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { NbPlaygroundComponent } from './playground.component';
+import { NbPlaygroundBaseComponent } from './playground-base.component';
+import { NbPlaygroundLayoutComponent } from './playground-layout.component';
 
 import { NbPopoverShowcaseComponent } from './popover/popover-showcase.component';
 import { NbPopoverTemplateRefComponent } from './popover/popover-template-ref.component';
@@ -17,12 +18,15 @@ import { NbPopoverModesComponent } from './popover/popover-modes.component';
 import { NbCheckboxShowcaseComponent } from './checkbox/checkbox-showcase.component';
 import { NbCheckboxStatusComponent } from './checkbox/checkbox-status.component';
 import { NbCheckboxDisabledComponent } from './checkbox/checkbox-disabled.component';
-
+import { NbLayoutShowcaseComponent } from './layout/layout-showcase.component';
+import { NbLayoutWFooterComponent } from './layout/layout-w-footer.component';
+import { NbLayoutFixedHeaderComponent } from './layout/layout-fixed-header.component';
+import { NbLayoutColumnLeftComponent } from './layout/layout-column-left.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: NbPlaygroundComponent,
+    component: NbPlaygroundLayoutComponent,
     children: [
       {
         path: 'popover-showcase/popover-showcase.component',
@@ -55,6 +59,28 @@ export const routes: Routes = [
       {
         path: 'checkbox-disabled/checkbox-disabled.component',
         component: NbCheckboxDisabledComponent,
+      },
+    ],
+  },
+  {
+    path: 'n',
+    component: NbPlaygroundBaseComponent,
+    children: [
+      {
+        path: 'layout-showcase/layout-showcase.component',
+        component: NbLayoutShowcaseComponent,
+      },
+      {
+        path: 'layout-w-footer/layout-w-footer.component',
+        component: NbLayoutWFooterComponent,
+      },
+      {
+        path: 'layout-fixed-header/layout-fixed-header.component',
+        component: NbLayoutFixedHeaderComponent,
+      },
+      {
+        path: 'layout-column-left/layout-column-left.component',
+        component: NbLayoutColumnLeftComponent,
       },
     ],
   },
