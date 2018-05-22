4d9c2e4eb6fbc7a941ea6bbc83a78595626b4824
Dmitry Nehaychik <4dmitr@gmail.com>
2018-05-22 13:24:31 +0300

    feat(docs): fix layout examples

--- src/playground/layout/layout-column-left.component.ts
+++ src/playground/layout/layout-column-left.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nb-layout-column-left',
   templateUrl: './layout-column-left.component.html',
+  styles: [`
+    ::ng-deep nb-layout-column {
+      height: 50vw;
+    }
+  `],
 })
 
 export class NbLayoutColumnLeftComponent {
