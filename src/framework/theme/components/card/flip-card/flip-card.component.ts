05abd6581523090228fb49ed9869c6f40fdfb24e
Dmitry Nehaychik <4dmitr@gmail.com>
2018-05-22 15:42:48 +0300

    feat(playground): flip & reveal cards

--- src/framework/theme/components/card/flip-card/flip-card.component.ts
+++ src/framework/theme/components/card/flip-card/flip-card.component.ts
@@ -1,22 +1,47 @@
 import { Component, Input, HostBinding } from '@angular/core';
 
 /**
- * Flip card component.
  *
- * ![image](assets/images/components/flip-card.gif)
+ * Flip card example:
+ * @example(flip-card/flip-card-showcase.component)
  *
- * @example
+ * As a content Flip card accepts two instances of `nb-card` - for front and back sides.
+ *
+ * Basic flip card configuration:
  *
  * ```html
  * <nb-flip-card>
  *   <nb-card-front>
- *     <nb-card><nb-card-body>Front Card</nb-card-body></nb-card>
+ *     <nb-card>
+ *       <nb-card-body>
+ *         Front
+ *       </nb-card-body>
+ *     </nb-card>
  *   </nb-card-front>
  *   <nb-card-back>
- *     <nb-card><nb-card-body>Back Card</nb-card-body></nb-card>
+ *     <nb-card>
+ *       <nb-card-body>
+ *         Back
+ *       </nb-card-body>
+ *     </nb-card>
  *   </nb-card-back>
  * </nb-flip-card>
  * ```
+ *
+ * Flip Card with header and footer:
+ * @example(flip-card/flip-card-full.component)
+ *
+ * Colored flip-cards could be simply configured by providing a `status` property:
+ * @example(flip-card/flip-card-colors.component)
+ *
+ * It is also possible to assign an `accent` property for a slight card highlight
+ * as well as combine it with `status`:
+ * @example(flip-card/flip-card-accents.component)
+ *
+ * @more-live-examples
+ *
+ * flip-card/flip-card-sizes.component
+ *
  */
 @Component({
   selector: 'nb-flip-card',
--- src/playground/playground-routing.module.ts
+++ src/playground/playground-routing.module.ts
@@ -32,6 +32,16 @@ import { NbCardFullComponent } from './card/card-full.component';
 import { NbCardColorsComponent } from './card/card-colors.component';
 import { NbCardAccentsComponent } from './card/card-accents.component';
 import { NbCardSizesComponent } from './card/card-sizes.component';
+import { NbFlipCardShowcaseComponent } from './flip-card/flip-card-showcase.component';
+import { NbFlipCardColorsComponent } from './flip-card/flip-card-colors.component';
+import { NbFlipCardAccentsComponent } from './flip-card/flip-card-accents.component';
+import { NbFlipCardSizesComponent } from './flip-card/filp-card-sizes.component';
+import { NbFlipCardFullComponent } from './flip-card/filp-card-full.component';
+import { NbRevealCardShowcaseComponent } from './reveal-card/reveal-card-showcase.component';
+import { NbRevealCardColorsComponent } from './reveal-card/reveal-card-colors.component';
+import { NbRevealCardAccentsComponent } from './reveal-card/reveal-card-accents.component';
+import { NbRevealCardSizesComponent } from './reveal-card/reveal-card-sizes.component';
+import { NbRevealCardFullComponent } from './reveal-card/reveal-card-full.component';
 
 export const routes: Routes = [
   {
@@ -89,6 +99,50 @@ export const routes: Routes = [
       {
         path: 'card/card-sizes.component',
         component: NbCardSizesComponent,
+      },
+      {
+        path: 'flip-card/flip-card-showcase.component',
+        component: NbFlipCardShowcaseComponent,
+      },
+      {
+        path: 'flip-card/flip-card-full.component',
+        component: NbFlipCardFullComponent,
+      },
+      {
+        path: 'flip-card/flip-card-colors.component',
+        component: NbFlipCardColorsComponent,
+      },
+      {
+        path: 'flip-card/flip-card-accents.component',
+        component: NbFlipCardAccentsComponent,
+      },
+      {
+        path: 'flip-card/flip-card-sizes.component',
+        component: NbFlipCardSizesComponent,
+      },
+      {
+        path: 'reveal-card/reveal-card-full.component',
+        component: NbRevealCardFullComponent,
+      },
+      {
+        path: 'reveal-card/reveal-card-showcase.component',
+        component: NbRevealCardShowcaseComponent,
+      },
+      {
+        path: 'reveal-card/reveal-card-colors.component',
+        component: NbRevealCardColorsComponent,
+      },
+      {
+        path: 'reveal-card/reveal-card-accents.component',
+        component: NbRevealCardAccentsComponent,
+      },
+      {
+        path: 'reveal-card/reveal-card-sizes.component',
+        component: NbRevealCardSizesComponent,
+      },
+      {
+        path: 'reveal-card/reveal-card-full.component',
+        component: NbRevealCardFullComponent,
       },
     ],
   },
