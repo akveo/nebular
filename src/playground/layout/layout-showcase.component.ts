d6bc4a1e49a4253e05e767700006cd2928f5a508
Dmitry Nehaychik <4dmitr@gmail.com>
2018-05-22 13:24:31 +0300

    feat(docs): fix layout examples

--- src/playground/layout/layout-showcase.component.ts
+++ src/playground/layout/layout-showcase.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nb-layout-showcase',
   templateUrl: './layout-showcase.component.html',
+  styles: [`
+    ::ng-deep nb-layout-column {
+      height: 50vw;
+    }
+  `],
 })
 
 export class NbLayoutShowcaseComponent {
--- src/playground/layout/layout-w-footer.component.ts
+++ src/playground/layout/layout-w-footer.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nb-w-footer-showcase',
   templateUrl: './layout-w-footer.component.html',
+  styles: [`
+    ::ng-deep nb-layout-column {
+      height: 50vw;
+    }
+  `],
 })
 
 export class NbLayoutWFooterComponent {
