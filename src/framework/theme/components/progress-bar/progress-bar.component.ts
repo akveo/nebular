60a3ec24d5d75d3849f7b73072d12670bf8ffa4b
Alexander Demeshko <demeshko.alexander.v@gmail.com>
2018-05-28 19:38:58 +0300

    feat(progress-bar): update examples

--- src/framework/theme/components/progress-bar/progress-bar.component.ts
+++ src/framework/theme/components/progress-bar/progress-bar.component.ts
@@ -7,42 +7,44 @@
 import {Component, Input} from '@angular/core';
 
 /**
- * <nb-progress-bar> is a component for indicating progress.
+ * Progress Bar is a component for indicating progress.
  *
- * Basic usage:
+ * Simple usage:
  *
  * ```html
  * <nb-progress-bar [value]="50"></nb-progress-bar>
  * ```
  *
- * Custom label:
+ * @stacked-example(Progress bar, progress-bar/progress-bar-showcase.component)
  *
- * ```html
- * <nb-progress-bar [value]="90">Almost Done!</nb-progress-bar>
- * ```
+ * @stacked-example(Progress bar types, progress-bar/progress-bar-type.component)
+ *
+ * @stacked-example(Progress bar sizes, progress-bar/progress-bar-size.component)
+ *
+ * @stacked-example(Progress bar values, progress-bar/progress-bar-value.component)
  *
  * @styles
  *
- * progress-bar-height-xlg
- * progress-bar-height-lg
- * progress-bar-height
- * progress-bar-height-sm
- * progress-bar-height-xs
- * progress-bar-font-size-xlg
- * progress-bar-font-size-lg
- * progress-bar-font-size
- * progress-bar-font-size-sm
- * progress-bar-font-size-xs
- * progress-bar-radius
- * progress-bar-bg-color
- * progress-bar-font-color
- * progress-bar-font-weight
- * progress-bar-default-bg-color
- * progress-bar-primary-bg-color,
- * progress-bar-success-bg-color,
- * progress-bar-info-bg-color
- * progress-bar-warning-bg-color,
- * progress-bar-danger-bg-color
+ * progress-bar-height-xlg:
+ * progress-bar-height-lg:
+ * progress-bar-height:
+ * progress-bar-height-sm:
+ * progress-bar-height-xs:
+ * progress-bar-font-size-xlg:
+ * progress-bar-font-size-lg:
+ * progress-bar-font-size:
+ * progress-bar-font-size-sm:
+ * progress-bar-font-size-xs:
+ * progress-bar-radius:
+ * progress-bar-bg-color:
+ * progress-bar-font-color:
+ * progress-bar-font-weight:
+ * progress-bar-default-bg-color:
+ * progress-bar-primary-bg-color:
+ * progress-bar-success-bg-color:
+ * progress-bar-info-bg-color:
+ * progress-bar-warning-bg-color:
+ * progress-bar-danger-bg-color:
  */
 @Component({
   selector: 'nb-progress-bar',
@@ -59,7 +61,7 @@ import {Component, Input} from '@angular/core';
 export class NbProgressBarComponent {
 
   /**
-   * Progress bar value in percent(0 - 100)
+   * Progress bar value in percent (0 - 100)
    * @type {number}
    * @private
    */
