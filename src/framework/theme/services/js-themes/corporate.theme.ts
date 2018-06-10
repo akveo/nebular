24563cba2494f7766c605b2340ad520e95c5e660
Denis Strigo <d.strigo@akveo.com>
2018-06-10 13:46:25 +0300

    feat(theme): add new corporate theme

---
+++ src/framework/theme/services/js-themes/corporate.theme.ts
@@ -0,0 +1,10 @@
+import { NbJSThemeOptions } from './theme.options';
+
+export const CORPORATE_THEME: NbJSThemeOptions = {
+  name: 'corporate',
+  base: 'default',
+
+  variables: {
+    fg: '#f1f5f8',
+  },
+};
