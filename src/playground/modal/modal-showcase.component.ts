9089c84294222e3a42679b8b6bbae0e49ff7e75c
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-06-11 11:01:24 +0300

    feat(theme): fix modal showcase

--- src/playground/modal/modal-showcase.component.ts
+++ src/playground/modal/modal-showcase.component.ts
@@ -4,7 +4,7 @@ import { NbModalDirective } from '@nebular/theme/components/modal/modal.directiv
 @Component({
   selector: 'nb-modal-showcase',
   template: `
-    <ng-template #modal [nbModal]="modal">
+    <ng-template #modal [nbModal]="modal" [nbModalBackdropClose]="true">
       <nb-card [style.width.px]="600">
         <nb-card-body>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/w9siwHUuUBg?autoplay=1"
@@ -13,32 +13,17 @@ import { NbModalDirective } from '@nebular/theme/components/modal/modal.directiv
         </nb-card-body>
       </nb-card>
     </ng-template>
-    <ng-template #closable [nbModal]="modal" [nbModalBackdropClose]="true">
-      <nb-card [style.width.px]="600">
-        <nb-card-body>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/w9siwHUuUBg?autoplay=1"
-                  frameborder="0"
-                  allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        </nb-card-body>
-      </nb-card>
-    </ng-template>
-    <button class="btn btn-primary" (click)="open()">Open</button>
-    <button class="btn btn-primary" (click)="openClosable()">Closable by backdrop click</button>
+    <button class="btn btn-primary" (click)="open()">Open Modal</button>
   `,
 })
 
 export class NbModalShowcaseComponent {
   @ViewChild('modal', { read: NbModalDirective }) modal: NbModalDirective;
-  @ViewChild('closable', { read: NbModalDirective }) closable: NbModalDirective;
 
   constructor() {
   }
 
   open() {
     this.modal.show();
-  }
-
-  openClosable() {
-    this.closable.show();
   }
 }
