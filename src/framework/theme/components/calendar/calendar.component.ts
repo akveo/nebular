b83c3908b843e8ab2de87b9d78e59af135630678
KostyaDanovsky <kostya.danovsky@gmail.com>
2018-06-11 15:37:58 +0300

    feat(calendar): add calendar to docs

--- src/framework/theme/components/calendar/calendar.component.ts
+++ src/framework/theme/components/calendar/calendar.component.ts
@@ -19,6 +19,39 @@ const defaultStartYear = 2016;
 const defaultYearCount = 20;
 
 /**
+ * Basic example with including bounding dates
+ * @stacked-example(Calendar Date Picker, calendar/calendar-test.component)
+ *
+ * Example with range selection
+ * @stacked-example(Calendar Date Range Picker, calendar/calendar-range-test.component)
+ *
+ * @styles
+ *
+ * calendar-width:
+ * calendar-height:
+ * calendar-padding:
+ * calendar-fg:
+ * calendar-bg:
+ * calendar-selected-item:
+ * calendar-active-item:
+ * calendar-between-item:
+ * calendar-hovered-item:
+ * calendar-inactive-opacity:
+ * calendar-cell-width:
+ * calendar-cell-height:
+ * calendar-month-cell-width:
+ * calendar-month-cell-height:
+ * calendar-year-cell-width:
+ * calendar-year-cell-height:
+ * calendar-border-radius:
+ * calendar-row-padding:
+ * calendar-weekday-width:
+ * calendar-weekday-height:
+ * calendar-weekday-font-size:
+ * calendar-weekday-font-weight:
+ * calendar-weekday-fg:
+ * calendar-holiday-fg:
+ *
  */
 @Component({
   selector: 'nb-calendar',
