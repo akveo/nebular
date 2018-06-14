2f28cbc073a8615236a3e0fbeef4c69ccc34b40d
Dmitry Nehaychik <4dmitr@gmail.com>
2018-06-14 19:36:35 +0300

    feat(auth): add OAuth2 strategy
    basic configuration example:
    ```
    strategies: [
            NbOAuth2AuthStrategy.setup({
              name: 'google',
              clientId: 'YOUR_CLIENT_ID',
              clientSecret: '',
              authorize: {
                endpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
                responseType: NbOAuth2ResponseType.TOKEN,
                scope: 'https://www.googleapis.com/auth/userinfo.profile',
                
                
                redirectUri: 'http://localhost:4100/example/oauth2/callback',
              },
            }),
          ],
    ```

--- docs/structure.ts
+++ docs/structure.ts
@@ -465,6 +465,17 @@ export const structure = [
       },
       {
         type: 'page',
+        name: 'NbOAuth2AuthStrategy',
+        children: [
+          {
+            type: 'block',
+            block: 'component',
+            source: 'NbOAuth2AuthStrategy',
+          },
+        ],
+      },
+      {
+        type: 'page',
         name: 'NbDummyAuthStrategy',
         children: [
           {
