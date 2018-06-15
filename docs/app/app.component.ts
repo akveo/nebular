857314bfccaf05be7533396498ba4139c8122080
Dmitry Nehaychik <4dmitr@gmail.com>
2018-06-15 13:54:19 +0300

    refactor(docs): remove unnecessary commented code

--- docs/app/app.component.ts
+++ docs/app/app.component.ts
@@ -13,47 +13,4 @@ import { Component } from '@angular/core';
   `,
 })
 export class NgdAppComponent {
-
-  // private fragmentSubscription: Subscription;
-  //
-  //
-  // constructor(@Inject(NB_WINDOW) private window,
-  //             private docsService: DocsService,
-  //             private router: Router,
-  //             private route: ActivatedRoute,
-  //             private elementRef: ElementRef,
-  //             private analytics: Analytics) {
-  // }
-  //
-  // ngOnInit(): void {
-  //   this.analytics.trackPageViews();
-  // }
-
-  // // TODO: refactor this
-  // ngAfterViewInit() {
-  //   // this.fragmentSubscription = this.route.fragment
-  //   //   .merge(this.docsService.onFragmentClick())
-  //   //   .delay(1)
-  //   //   .subscribe(fr => this.processFragment(fr));
-  // }
-
-  // processFragment(fr) {
-  //   if (fr) {
-  //     const el = this.elementRef.nativeElement.querySelector(`#${fr}`);
-  //     if (el) {
-  //       el.scrollIntoView();
-  //       if (new RegExp(/themes/i).test(this.router.url)) {
-  //         this.window.scrollBy(0, -235); // header + theme-header
-  //       } else {
-  //         this.window.scrollBy(0, -80);
-  //       }
-  //     }
-  //   } else {
-  //     this.window.scrollTo(0, 0);
-  //   }
-  // }
-
-  // ngOnDestroy() {
-  //   // this.fragmentSubscription.unsubscribe();
-  // }
 }
