ca73f61c2e8162188f9a05aa86b11bab03a6367e
Dmitry Nehaychik <4dmitr@gmail.com>
2018-06-20 16:17:25 +0300

    fix(docs): fix widget margin

--- docs/app/blocks/components/overview-block/overview-block.component.ts
+++ docs/app/blocks/components/overview-block/overview-block.component.ts
@@ -9,11 +9,14 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
           <ng-container *ngIf="node.type === 'text'">
             <div *ngFor="let section of node.content" [innerHtml]="section.html"></div>
           </ng-container>
-          <ngd-live-example-block *ngIf="node.type === 'live-example'" [content]="node.content">
+          <ngd-live-example-block *ngIf="node.type === 'live-example'" [content]="node.content"
+                                  class="widget-block">
           </ngd-live-example-block>
-          <ngd-inline-example-block *ngIf="node.type === 'inline-example'" [content]="node.content">
+          <ngd-inline-example-block *ngIf="node.type === 'inline-example'" [content]="node.content"
+                                    class="widget-block">
           </ngd-inline-example-block>
-          <ngd-stacked-example-block *ngIf="node.type === 'stacked-example'" [content]="node.content">
+          <ngd-stacked-example-block *ngIf="node.type === 'stacked-example'" [content]="node.content"
+                                     class="widget-block">
           </ngd-stacked-example-block>
         </ng-container>
       </nb-card-body>
