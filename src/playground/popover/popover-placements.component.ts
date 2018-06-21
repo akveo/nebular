a80fe3a79d4a08b2fa77f2f0b152aa9fbb32d7d2
Dmitry Nehaychik <4dmitr@gmail.com>
2018-06-21 15:11:19 +0300

    fix(docs): fix popover examples

--- src/playground/popover/popover-placements.component.ts
+++ src/playground/popover/popover-placements.component.ts
@@ -10,6 +10,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nb-popover-placements',
   templateUrl: './popover-placements.component.html',
+  styles: [`
+    :host {
+      margin: 2rem 0;
+      display: block;
+    }
+    .btn {
+      margin: 1rem;
+    }
+  `],
 })
 export class NbPopoverPlacementsComponent {
 }
