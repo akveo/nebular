c7dfde5ab1c9c73c5c8af8a666856ff5eae5802a
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-07-21 17:44:18 +0300

    refactor(calendar): refactor year picker

--- src/framework/theme/components/calendar/components/calendar-year-picker/calendar-year-picker.component.ts
+++ src/framework/theme/components/calendar/components/calendar-year-picker/calendar-year-picker.component.ts
@@ -10,20 +10,19 @@ import {
   EventEmitter,
   Input,
   OnChanges,
-  OnInit,
   Output,
   SimpleChanges,
 } from '@angular/core';
 
 import { NbDateTimeUtil } from '../../service/date-time-util';
-import { batch } from '../../helpers';
+import { batch, range } from '../../helpers';
 import { NbCalendarConfig } from '../../calendar-config';
 
+// TODO i don't think we need defaults
 const defaultStartYear = 2016;
 const defaultYearCount = 16;
 
-/**
- */
+// TODO refactor template with styles refactoring
 @Component({
   selector: 'nb-calendar-year-picker',
   styleUrls: ['./calendar-year-picker.component.scss'],
@@ -40,7 +39,7 @@ const defaultYearCount = 16;
         <div class="year"
              *ngFor="let year of chunk"
              [class.selected]="year.selected"
-             (click)="onYearSelect(year.value)">
+             (click)="onSelect(year.value)">
           {{ year.value }}
         </div>
       </div>
@@ -48,7 +47,7 @@ const defaultYearCount = 16;
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NbCalendarYearPickerComponent<D> implements OnInit, OnChanges {
+export class NbCalendarYearPickerComponent<D> implements OnChanges {
 
   @Input() activeMonth: D;
   @Input() startYear: number = defaultStartYear;
@@ -69,13 +68,8 @@ export class NbCalendarYearPickerComponent<D> implements OnInit, OnChanges {
     return this.config.yearsToDisplayNumber || defaultYearCount;
   }
 
-  ngOnInit() {
-    this.today = this.today || this.dateTimeUtil.createNowDate();
-    this.activeMonth = this.activeMonth || this.dateTimeUtil.clone(this.today);
-  }
-
-
   ngOnChanges(changes: SimpleChanges) {
+    // TODO I'm not sure we need this
     if (this.activeMonth && this.startYear && this.yearsToDisplay) {
       this.initYears();
     }
@@ -84,7 +78,8 @@ export class NbCalendarYearPickerComponent<D> implements OnInit, OnChanges {
   initYears() {
     const selectedYear = this.dateTimeUtil.getYear(this.activeMonth);
 
-    const years = Array.from(Array(this.yearsToDisplay).keys())
+    // TODO maybe we need one more util for cases like that?
+    const years = range(this.yearsToDisplay)
       .map(index => ({
         value: this.startYear + index,
         selected: selectedYear === this.startYear + index,
@@ -93,12 +88,10 @@ export class NbCalendarYearPickerComponent<D> implements OnInit, OnChanges {
     this.years = batch(years, 4);
   }
 
-  onYearSelect(year) {
-    this.activeMonth = this.dateTimeUtil.createDate(
-      year,
-      this.dateTimeUtil.getMonth(this.activeMonth),
-      this.dateTimeUtil.getDate(this.activeMonth),
-    );
-    this.change.emit(this.activeMonth);
+  onSelect(year) {
+    const month = this.dateTimeUtil.getMonth(this.activeMonth);
+    const day = this.dateTimeUtil.getDate(this.activeMonth);
+    const event = this.dateTimeUtil.createDate(year, month, day);
+    this.change.emit(event);
   }
 }
