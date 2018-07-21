1f2ad58499aa0858023ddc87af34a29464519c83
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-07-21 13:29:23 +0300

    fix(calendar): remove redundand package-lock.json

--- docs/polyfills.ts
+++ docs/polyfills.ts
@@ -68,7 +68,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  */
 
 /**
- * Date, currency, decimal and percent helpers.
+ * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
