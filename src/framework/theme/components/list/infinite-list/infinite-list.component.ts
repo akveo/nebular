cee0099a47fd7e5819626c062075b8a5da157542
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2018-07-23 20:24:29 +0300

    refactor(infinite list): remove buttons

--- src/framework/theme/components/list/infinite-list/infinite-list.component.ts
+++ src/framework/theme/components/list/infinite-list/infinite-list.component.ts
@@ -1,54 +1,4 @@
-import {
-  Component,
-  Input,
-  Output,
-  EventEmitter,
-  HostBinding,
-  OnDestroy,
-  ContentChild,
-  Directive,
-  HostListener,
-  ChangeDetectorRef,
-  AfterViewChecked,
-} from '@angular/core';
-import { Subject, Observable, Subscription } from 'rxjs';
-import { takeWhile } from 'rxjs/operators';
-
-@Directive({
-  selector: 'button[nbDisableAutoLoadButton]',
-})
-export class NbDisableAutoLoadButtonDirective {
-  private clicked = new Subject();
-
-  get click(): Observable<any> {
-    return this.clicked.asObservable();
-  }
-
-  @HostBinding('attr.aria-pressed')
-  pressed = false;
-
-  @HostListener('click')
-  emitClick() {
-    this.pressed = !this.pressed;
-    this.clicked.next();
-  }
-}
-
-@Directive({
-  selector: 'button[nbLoadMoreButtonDirective]',
-})
-export class NbLoadMoreButtonDirective {
-  private clicked = new Subject();
-
-  get click() {
-    return this.clicked.asObservable();
-  }
-
-  @HostListener('click')
-  emitClick() {
-    this.clicked.next();
-  }
-}
+import { Component, Input, Output, EventEmitter, HostBinding, OnDestroy } from '@angular/core';
 
 /**
  * Infinite list component.
@@ -60,29 +10,17 @@ export class NbLoadMoreButtonDirective {
 @Component({
   selector: 'nb-infinite-list',
   template: `
-    <ng-content select="[nbDisableAutoLoadButton]"></ng-content>
-
     <nb-list
       [nbScrollThreshold]="loadMoreThreshold"
       [listenWindowScroll]="listenWindowScroll"
-      (bottomThresholdReached)="onBottomThresholdReached()"
-      (topThresholdReached)="onTopThresholdReached()">
+      (bottomThresholdReached)="emitLoadNext()"
+      (topThresholdReached)="emitLoadPrev()">
       <ng-content select="nb-list-item"></ng-content>
     </nb-list>
-
-    <ng-content select="[nbLoadMoreButtonDirective]"></ng-content>
   `,
   styleUrls: [ './infintie-list.component.scss' ],
 })
-export class NbInfiniteListComponent implements AfterViewChecked, OnDestroy {
-
-  private alive = true;
-
-  /**
-   * Enables emiting of load more event when treshold reached.
-   * When disabled load more event will be fired only after clicking on load more button.
-   */
-  @Input() autoLoading = true;
+export class NbInfiniteListComponent {
 
   /**
    * By default component observes self scroll position.
@@ -107,56 +45,11 @@ export class NbInfiniteListComponent implements AfterViewChecked, OnDestroy {
    * Emits when distance between bottom of the observed element and current scroll position is less than threshold.
    */
   @Output() loadNext = new EventEmitter();
+
   /**
    * Emits when distance between top of the observed element and current scroll position is less than threshold.
    */
   @Output() loadPrev = new EventEmitter();
-
-  @ContentChild(NbDisableAutoLoadButtonDirective) disableAutoLoadButton: NbDisableAutoLoadButtonDirective;
-  @ContentChild(NbLoadMoreButtonDirective) loadMoreButton: NbLoadMoreButtonDirective;
-  private disableClickSubscription: Subscription;
-  private loadMoreClickSubscription: Subscription;
-
-  constructor(private changeDetection: ChangeDetectorRef) {}
-
-  ngAfterViewChecked() {
-    if (this.disableClickSubscription) {
-      this.disableClickSubscription.unsubscribe();
-    }
-    if (this.loadMoreClickSubscription) {
-      this.loadMoreClickSubscription.unsubscribe();
-    }
-
-    if (this.disableAutoLoadButton) {
-      this.disableAutoLoadButton.pressed = !this.autoLoading;
-      this.disableClickSubscription = this.disableAutoLoadButton.click
-        .pipe(takeWhile(() => this.alive))
-        .subscribe(() => this.autoLoading = !this.autoLoading);
-    }
-    if (this.loadMoreButton) {
-      this.loadMoreClickSubscription = this.loadMoreButton.click
-        .pipe(takeWhile(() => this.alive))
-        .subscribe(() => this.emitLoadNext());
-    }
-
-    this.changeDetection.detectChanges();
-  }
-
-  ngOnDestroy() {
-    this.alive = false;
-  }
-
-  onTopThresholdReached() {
-    if (this.autoLoading) {
-      this.emitLoadPrev();
-    }
-  }
-
-  onBottomThresholdReached() {
-    if (this.autoLoading) {
-      this.emitLoadNext();
-    }
-  }
 
   emitLoadPrev() {
     this.loadPrev.emit();
@@ -164,9 +57,5 @@ export class NbInfiniteListComponent implements AfterViewChecked, OnDestroy {
 
   emitLoadNext() {
     this.loadNext.emit();
-  }
-
-  disableAutoLoading() {
-    this.autoLoading = false;
   }
 }
--- src/framework/theme/components/list/infinite-list/infinite-list.module.ts
+++ src/framework/theme/components/list/infinite-list/infinite-list.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NbListModule } from '../list.module';
-import {
-  NbInfiniteListComponent,
-  NbDisableAutoLoadButtonDirective,
-  NbLoadMoreButtonDirective,
-} from './infinite-list.component';
+import { NbInfiniteListComponent } from './infinite-list.component';
 import { NbScrollThresholdDirective } from './scroll-threshold.directive';
 
 const componets = [
   NbInfiniteListComponent,
   NbScrollThresholdDirective,
-  NbDisableAutoLoadButtonDirective,
-  NbLoadMoreButtonDirective,
 ];
 
 @NgModule({
