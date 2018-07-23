1ea500464e60c255cc8443c1cbec9f78b0d85abb
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-07-23 16:52:42 +0300

    feat(calendar): days-names spec

---
+++ src/framework/theme/components/calendar/components/calendar-days-names/calendar-days-names.component.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NbCalendarDaysNamesComponent } from './calendar-days-names.component';
+import { NbDateTimeUtil } from '../../service/date-time-util';
+import { NbNativeDateTimeUtilService } from '../../service/native-date-time-util.service';
+
+
+describe('Component: NbCalendarDaysNames', () => {
+  let component: NbCalendarDaysNamesComponent<Date>;
+  let fixture: ComponentFixture<NbCalendarDaysNamesComponent<Date>>;
+  let componentEl: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [NbCalendarDaysNamesComponent],
+      providers: [{ provide: NbDateTimeUtil, useClass: NbNativeDateTimeUtilService }],
+    });
+    fixture = TestBed.createComponent<NbCalendarDaysNamesComponent<Date>>(NbCalendarDaysNamesComponent);
+    component = fixture.componentInstance;
+    componentEl = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+  });
+
+  it('should render seven days', () => {
+    const days = componentEl.querySelectorAll('div');
+    expect(days.length).toBe(7);
+  });
+
+  it('should render two holidays', () => {
+    const days = componentEl.querySelectorAll('.holiday');
+    expect(days.length).toBe(2);
+  });
+
+  it('should contain narrow names', () => {
+    const days = componentEl.querySelectorAll('div');
+    [].forEach.call(days, day => {
+      expect(day.textContent.length).toBe(1);
+    });
+  });
+});
