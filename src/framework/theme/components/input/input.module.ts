42c588e39ce495cb62a6f5a8ddfdaffedaf393ca
Sergey Andrievskiy <yggg@users.noreply.github.com>
2018-07-25 17:11:00 +0300

    feat(theme): add new Input directive (#569)

---
+++ src/framework/theme/components/input/input.module.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+import { NgModule } from '@angular/core';
+import { NbSharedModule } from '../shared/shared.module';
+import { NbInputDirective } from './input.directive';
+
+const NB_INPUT_COMPONENTS = [
+  NbInputDirective,
+];
+
+@NgModule({
+  imports: [ NbSharedModule ],
+  declarations: NB_INPUT_COMPONENTS,
+  exports: NB_INPUT_COMPONENTS,
+})
+export class NbInputModule {}
