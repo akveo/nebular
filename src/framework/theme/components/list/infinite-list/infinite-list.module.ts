e9e055e9a1cc920b9d0ce6068cdac7c56c882789
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2018-07-25 23:37:20 +0300

    style: remove unused import

--- src/framework/theme/components/list/infinite-list/infinite-list.module.ts
+++ src/framework/theme/components/list/infinite-list/infinite-list.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { NbListModule } from '../list.module';
 import { NbInfiniteListDirective } from './infinite-list.directive';
 
