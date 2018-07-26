11a4a3938724d69a62730f45809030a53c0ce0ba
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2018-07-26 23:25:14 +0300

    feat(infinite list): add directive module exports

--- src/framework/theme/index.ts
+++ src/framework/theme/index.ts
@@ -61,5 +61,6 @@ export * from './components/button/button.module';
 export * from './components/list/list.component';
 export * from './components/list/list.module';
 export * from './components/list/list-pager.directive';
+export * from './components/list/infinite-list.directive';
 export * from './components/input/input.directive';
 export * from './components/input/input.module';
