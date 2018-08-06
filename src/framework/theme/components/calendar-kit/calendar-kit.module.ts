b5bc794a3d432c7e06b5e6e197499401985ed292
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-08-06 15:52:05 +0300

    feat(calendar): add list of available

--- src/framework/theme/components/calendar-kit/calendar-kit.module.ts
+++ src/framework/theme/components/calendar-kit/calendar-kit.module.ts
@@ -55,6 +55,17 @@ const PIPES = [
  * `NbCalendarKitModule` is a module that contains multiple useful components for building custom calendars.
  * So if you think our calendars is not enough powerful for you just use calendar-kit and build your own calendar!
  *
+ * Available components:
+ * - `NbCalendarDayPicker`
+ * - `NbCalendarDayCell`
+ * - `NbCalendarMonthPicker`
+ * - `NbCalendarMonthCell`
+ * - `NbCalendarYearPicker`
+ * - `NbCalendarYearCell`
+ * - `NbCalendarHeader`
+ * - `NbCalendarNavigation`
+ * - `NbCalendarPageableNavigation`
+ *
  * For example you can easily build full calendar:
  * @stacked-example(Full calendar, calendar-kit/calendar-kit-full-calendar.component)
  * */
