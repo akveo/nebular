0ae51f61d57385655f4a68be74575ff100caa615
alain.charles <alain.charles@cerema.fr>
2018-08-07 14:18:24 +0200

    added parameter type = NbAuthToken in exported const NB_AUTH_TOKENS.

--- src/framework/auth/auth.options.ts
+++ src/framework/auth/auth.options.ts
@@ -1,6 +1,6 @@
 import { InjectionToken } from '@angular/core';
 import { NbAuthStrategy, NbAuthStrategyOptions } from './strategies';
-import { NbAuthTokenClass } from './services';
+import { NbAuthToken, NbAuthTokenClass } from './services/token/token';
 
 export type NbAuthStrategyClass = new (...params: any[]) => NbAuthStrategy;
 
@@ -87,5 +87,5 @@ export const defaultAuthOptions: any = {
 export const NB_AUTH_OPTIONS = new InjectionToken<NbAuthOptions>('Nebular Auth Options');
 export const NB_AUTH_USER_OPTIONS = new InjectionToken<NbAuthOptions>('Nebular User Auth Options');
 export const NB_AUTH_STRATEGIES = new InjectionToken<NbAuthStrategies>('Nebular Auth Strategies');
-export const NB_AUTH_TOKENS = new InjectionToken<NbAuthTokenClass[]>('Nebular Auth Tokens');
+export const NB_AUTH_TOKENS = new InjectionToken<NbAuthTokenClass<NbAuthToken>[]>('Nebular Auth Tokens');
 export const NB_AUTH_INTERCEPTOR_HEADER = new InjectionToken<NbAuthStrategies>('Nebular Simple Interceptor Header');
