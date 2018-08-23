b4fc62481c89a0223b9502bef2a61cd45250b56c
Alain CHARLES <40032406+alain-charles@users.noreply.github.com>
2018-08-23 20:56:28 +0200

    feat(playground): automatic refresh token (#658)
    This can be played in the playground with `NbAuthPasswordStrategy` as well, just replace in the ** playground**  `auth-module.ts`:
    ```
    login: {
              strategy: 'password',
    ...}
    ```
    by 
    ```
    login: {
              strategy: 'email',
    ...}
    ```

--- src/backend/auth.js
+++ src/backend/auth.js
@@ -12,12 +12,12 @@ var ExtractJwt = passportJWT.ExtractJwt;
 var Strategy = passportJWT.Strategy;
 var params = {
   secretOrKey: cfg.jwtSecret,
-  jwtFromRequest: ExtractJwt.fromAuthHeader()
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
 };
 
 module.exports = function () {
   var strategy = new Strategy(params, function (payload, done) {
-    var user = users[payload.id] || null;
+    var user = users[payload.sub -1 ] || null;
     if (user) {
       return done(null, {
         id: user.id
---
+++ src/playground/auth/api-calls/wine.ts
@@ -0,0 +1,6 @@
+export interface Wine {
+  id: number;
+  name: string;
+  region: string;
+  year: number
+}
