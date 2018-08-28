60d64b71f07d6730b0787e3bfc5b7720327c4526
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-08-28 13:33:22 +0300

    feat(modal): add docs on NbModalConfig

--- src/framework/theme/components/modal/modal.service.ts
+++ src/framework/theme/components/modal/modal.service.ts
@@ -8,20 +8,40 @@ import {
   NbGlobalPositionStrategy,
   NbOverlayRef,
   NbOverlayService,
-  NbPositionBuilderService, NbScrollStrategy,
+  NbPositionBuilderService,
+  NbScrollStrategy,
 } from '../cdk';
 import { NB_DOCUMENT } from '../../theme.options';
 
 
+/**
+ * Describes all available options that may be passed to the NbModalService.
+ * */
 export class NbModalConfig {
+  /**
+   * If true than overlay will render backdrop under a modal.
+   * */
   hasBackdrop?: boolean = true;
-  backdropClass?: string = 'overlay-backdrop';
-  closeOnBackdropClick?: boolean = true;
-  closeOnEsc?: boolean = true;
-  hasScroll?: boolean = false;
 
-  // TODO do we need it???
-  context?: Object;
+  /**
+   * Class that'll be assigned to the backdrop element.
+   * */
+  backdropClass?: string = 'overlay-backdrop';
+
+  /**
+   * If true then mouse clicks by backdrop will close a modal.
+   * */
+  closeOnBackdropClick?: boolean = true;
+
+  /**
+   * If true then escape press will close a modal.
+   * */
+  closeOnEsc?: boolean = true;
+
+  /**
+   * Disables scroll on content under modal if true and does nothing otherwise.
+   * */
+  hasScroll?: boolean = false;
 
   constructor(config: Partial<NbModalConfig>) {
     Object.assign(this, config);
@@ -80,16 +100,18 @@ export class NbModalService {
               @Inject(NB_DOCUMENT) protected document) {
   }
 
-  show<T>(component: NbComponentType<T>, config: Partial<NbModalConfig> = {}): NbModalRef<T> {
-    config = new NbModalConfig(config);
+  show<T>(component: NbComponentType<T>, userConfig: Partial<NbModalConfig> = {}): NbModalRef<T> {
+    const config = new NbModalConfig(userConfig);
     const overlayRef = this.createOverlay(config);
     const componentRef = overlayRef.attach(new NbComponentPortal(component));
+
     return new NbModalRef(overlayRef, componentRef, config, this.document);
   }
 
   protected createOverlay(config: NbModalConfig): NbOverlayRef {
     const positionStrategy = this.createPositionStrategy();
     const scrollStrategy = this.createScrollStrategy(config.hasScroll);
+
     return this.overlay.create({
       positionStrategy,
       scrollStrategy,
