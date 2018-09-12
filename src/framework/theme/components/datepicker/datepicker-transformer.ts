42eb6e9f479c5c27aec40b7837d41d9a08a36520
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-09-12 17:48:33 +0300

    refactor(datepicker): move transformers in separate file

---
+++ src/framework/theme/components/datepicker/datepicker-transformer.ts
@@ -0,0 +1,48 @@
+/*
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+import { Injectable, Type } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { NbCalendarRange } from '../calendar/calendar-range.component';
+import { NbDatepickerComponent, NbRangepickerComponent } from './datepicker.component';
+import { NbDateTransformer } from './datepicker.directive';
+
+
+@Injectable()
+export class NbDateTransformerService extends NbDateTransformer<Date> {
+  picker: Type<NbDatepickerComponent> = NbDatepickerComponent;
+
+  constructor(protected datePipe: DatePipe) {
+    super();
+  }
+
+  fromString(value: string): Date {
+    return undefined;
+  }
+
+  fromValue(value: Date): string {
+    return this.datePipe.transform(value);
+  }
+}
+
+@Injectable()
+export class NbRangeTransformerService extends NbDateTransformer<NbCalendarRange> {
+  picker: Type<NbRangepickerComponent> = NbRangepickerComponent;
+
+  constructor(protected datePipe: DatePipe) {
+    super();
+  }
+
+  fromString(value: string): NbCalendarRange {
+    return undefined;
+  }
+
+  fromValue(range: NbCalendarRange): string {
+    return `${this.datePipe.transform(range.start)} - ${this.datePipe.transform(range.end)}`;
+  }
+}
+
--- src/framework/theme/components/datepicker/datepicker.module.ts
+++ src/framework/theme/components/datepicker/datepicker.module.ts
@@ -4,50 +4,18 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-import { Injectable, NgModule, Type } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
-import { NB_DATE_TRANSFORMER, NbDatepickerDirective, NbDateTransformer } from './datepicker.directive';
+import { NB_DATE_TRANSFORMER, NbDatepickerDirective } from './datepicker.directive';
 import { NbOverlayModule } from '../cdk';
 import { NbCalendarModule } from '../calendar/calendar.module';
 import { NbCalendarComponent } from '../calendar/calendar.component';
 import { NbDatepickerContainerComponent } from './datepicker-container.component';
 import { NbDatepickerComponent, NbRangepickerComponent } from './datepicker.component';
-import { DatePipe } from '@angular/common';
-import { NbCalendarRange, NbCalendarRangeComponent } from '@nebular/theme';
+import { NbCalendarRangeComponent } from '../calendar/calendar-range.component';
+import { NbDateTransformerService, NbRangeTransformerService } from './datepicker-transformer';
 
-@Injectable()
-export class NbDateTransformerService extends NbDateTransformer<Date> {
-  picker: Type<NbDatepickerComponent> = NbDatepickerComponent;
-
-  constructor(protected datePipe: DatePipe) {
-    super();
-  }
-
-  fromString(value: string): Date {
-    return undefined;
-  }
-
-  fromValue(value: Date): string {
-    return this.datePipe.transform(value);
-  }
-}
-
-@Injectable()
-export class NbRangeTransformerService extends NbDateTransformer<NbCalendarRange> {
-  picker: Type<NbRangepickerComponent> = NbRangepickerComponent;
-
-  constructor(protected datePipe: DatePipe) {
-    super();
-  }
-
-  fromString(value: string): NbCalendarRange {
-    return undefined;
-  }
-
-  fromValue(range: NbCalendarRange): string {
-    return `${this.datePipe.transform(range.start)} - ${this.datePipe.transform(range.end)}`;
-  }
-}
 
 @NgModule({
   imports: [NbOverlayModule, NbCalendarModule],
