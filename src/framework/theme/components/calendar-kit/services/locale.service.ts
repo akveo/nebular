a56446136772c0d0782a696011522714bb390646
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-09-13 11:35:25 +0300

    refactor(calendar): add locale service

--- src/framework/theme/components/calendar-kit/services/locale.service.ts
+++ src/framework/theme/components/calendar-kit/services/locale.service.ts
@@ -1,46 +1,146 @@
-/**
+/*
  * @license
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-import { Inject, Injectable, LOCALE_ID } from '@angular/core';
-import {
-  FormStyle,
-  getLocaleDayNames,
-  getLocaleFirstDayOfWeek,
-  getLocaleMonthNames,
-  TranslationWidth,
-} from '@angular/common';
+import { TranslationWidth } from '@angular/common';
 
 
-@Injectable()
-export class NbLocaleService {
+export abstract class NbLocaleService<D> {
   protected locale: string;
+  protected DAYS_IN_WEEK: number = 7;
 
-  constructor(@Inject(LOCALE_ID) locale: string) {
-    this.locale = locale;
+  /**
+   * Checks if the date is between the start date and the end date.
+   * */
+  isBetween(date: D, start: D, end: D): boolean {
+    return this.compareDates(date, start) > 0 && this.compareDates(date, end) < 0;
+  };
+
+  /**
+   * Checks is two dates have the same day.
+   * */
+  isSameDaySafe(date1: D, date2: D): boolean {
+    return date1 && date2 && this.isSameDay(date1, date2);
+  };
+
+  /**
+   * Checks is two dates have the same month.
+   * */
+  isSameMonthSafe(date1: D, date2: D): boolean {
+    return date1 && date2 && this.isSameMonth(date1, date2);
   }
 
   /**
-   * returns first day of the week, it can be 1 if week starts from monday
+   * Checks is two dates have the same year.
+   * */
+  isSameYearSafe(date1: D, date2: D): boolean {
+    return date1 && date2 && this.isSameYear(date1, date2);
+  }
+
+
+  /**
+   * Returns first day of the week, it can be 1 if week starts from monday
    * and 0 if from sunday and so on.
    * */
-  getFirstDayOfWeek(): number {
-    return getLocaleFirstDayOfWeek(this.locale);
-  }
+  abstract getFirstDayOfWeek(): number;
 
-  getMonthName(date: Date, style: TranslationWidth = TranslationWidth.Abbreviated): string {
-    const index: number = date.getMonth();
-    return this.getMonthNameByIndex(index, style);
-  }
+  /**
+   * Returns localized month name by date and style.
+   * */
+  abstract getMonthName(date: D, style: TranslationWidth): string;
 
-  getMonthNameByIndex(index: number, style: TranslationWidth = TranslationWidth.Abbreviated): string {
-    return getLocaleMonthNames(this.locale, FormStyle.Format, style)[index];
-  }
+  /**
+   * Returns localized month name by month index and style.
+   * */
+  abstract getMonthNameByIndex(index: number, style: TranslationWidth): string;
 
-  getDayOfWeekNames(): string[] {
-    return getLocaleDayNames(this.locale, FormStyle.Format, TranslationWidth.Narrow);
-  }
+  /**
+   * Returns localized days names.
+   * */
+  abstract getDayOfWeekNames(): string[];
 
+  /**
+   * Parses the date string according to the passed format.
+   * */
+  abstract parse(date: string, format: string): D;
+
+  /**
+   * Transforms the date to the string according to the passed format.
+   * */
+  abstract format(date: D, format: string): string;
+
+  /**
+   * Creates new date from year, month and date.
+   * */
+  abstract createDate(year: number, month: number, date: number): D;
+
+  /**
+   * Checks is two dates have the same year.
+   * */
+  abstract isSameYear(date1: D, date2: D): boolean;
+
+  /**
+   * Checks is two dates have the same month.
+   * */
+  abstract isSameMonth(date1: D, date2: D): boolean;
+
+  /**
+   * Checks is two dates have the same day.
+   * */
+  abstract isSameDay(date1: D, date2: D): boolean;
+
+  /**
+   * Compares two dates.
+   * Returns 0 if dates are the same.
+   * Returns 1 if the first date is greater than the second.
+   * Returns -1 if the second date is greater than the first.
+   * */
+  abstract compareDates(date1: D, date2: D): number;
+
+  /**
+   * Clones passed date.
+   * */
+  abstract clone(date: D): D;
+
+  /**
+   * Creates the same date but with day equals to 1.
+   * */
+  abstract getMonthStart(date: D): D;
+
+  /**
+   * Creates the same date but with day equals to the last day in this month.
+   * */
+  abstract getMonthEnd(date: D): D;
+
+  /**
+   * Creates the same date but with month equals to 0 and day equals to 1.
+   * */
+  abstract getYearStart(date: D): D;
+
+  /**
+   * Creates the same date but with month equals to 11 and day equals to 31.
+   * */
+  abstract getYearEnd(date: D): D;
+
+  /**
+   * Returns number of days in the date.
+   * */
+  abstract getNumberOfDaysInMonth(date: D): number;
+
+  /**
+   * Returns date with added number of days.
+   * */
+  abstract addDay(date: D, num: number): D;
+
+  /**
+   * Returns date with added number of months.
+   * */
+  abstract addMonth(date: D, num: number): D;
+
+  /**
+   * Returns date with added number of years.
+   * */
+  abstract addYear(date: D, num: number): D;
 }
