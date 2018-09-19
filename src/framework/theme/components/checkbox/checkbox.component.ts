6668d80172c805128920ac100fbdb8c9f415c4f9
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2018-09-19 14:25:08 +0300

    feat(checkbox): add outline on focus

--- src/framework/theme/components/checkbox/checkbox.component.ts
+++ src/framework/theme/components/checkbox/checkbox.component.ts
@@ -44,7 +44,9 @@ import { convertToBoolProperty } from '../helpers';
       <input type="checkbox" class="customised-control-input"
              [disabled]="disabled"
              [checked]="value"
-             (change)="value = !value">
+             (change)="value = !value"
+             (focus)="setFocus()"
+             (blur)="removeFocus()">
       <span class="customised-control-indicator"></span>
       <span class="customised-control-description">
         <ng-content></ng-content>
@@ -98,6 +100,9 @@ export class NbCheckboxComponent implements ControlValueAccessor {
     return this.status === 'danger';
   }
 
+  @HostBinding('class.focus')
+  focus: boolean = false;
+
   onChange: any = () => { };
   onTouched: any = () => { };
 
@@ -125,5 +130,13 @@ export class NbCheckboxComponent implements ControlValueAccessor {
 
   setDisabledState(val: boolean) {
     this.disabled = convertToBoolProperty(val);
+  }
+
+  setFocus() {
+    this.focus = true;
+  }
+
+  removeFocus() {
+    this.focus = false;
   }
 }
