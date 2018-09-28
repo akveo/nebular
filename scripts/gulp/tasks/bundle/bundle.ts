6f4cfc6febe78b2a90117091f92bc407d91f20e0
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-09-28 12:16:24 +0300

    refactor(scripts): add capability bundle packages by nae

--- scripts/gulp/tasks/bundle/bundle.ts
+++ scripts/gulp/tasks/bundle/bundle.ts
@@ -7,48 +7,20 @@ const rename = require('gulp-rename');
 const replace = require('gulp-replace');
 
 task('bundle', ['bundle:umd:theme', 'bundle:umd:auth', 'bundle:umd:security']);
-task('bundle:umd:theme', bundleUmdTheme);
-task('bundle:umd:auth', bundleUmdAuth);
-task('bundle:umd:security', bundleUmdSecurity);
+task('bundle:umd:theme', () => bundleUmdModule('theme'));
+task('bundle:umd:auth', () => bundleUmdModule('auth'));
+task('bundle:umd:security', () => bundleUmdModule('security'));
 task('bundle:rename-dev', bundleRenameDev);
 
-function bundleUmdTheme() {
-  const config = {
-    src: `${LIB_DIR}/theme/**/*.js`,
-    moduleName: 'nb.theme',
-    entry: `${LIB_DIR}/theme/index.js`,
+function bundleUmdModule(name: string) {
+  bundle({
+    src: `${LIB_DIR}/${name}/**/*.js`,
+    moduleName: `nb.${name}`,
+    entry: `${LIB_DIR}/${name}/index.js`,
     format: 'umd',
-    output: 'theme.umd.js',
-    dest: `${LIB_DIR}/theme/bundles`,
-  };
-
-  bundle(config);
-}
-
-function bundleUmdAuth() {
-  const config = {
-    src: `${LIB_DIR}/auth/**/*.js`,
-    moduleName: 'nb.auth',
-    entry: `${LIB_DIR}/auth/index.js`,
-    format: 'umd',
-    output: 'auth.umd.js',
-    dest: `${LIB_DIR}/auth/bundles`,
-  };
-
-  bundle(config);
-}
-
-function bundleUmdSecurity() {
-  const config = {
-    src: `${LIB_DIR}/security/**/*.js`,
-    moduleName: 'nb.security',
-    entry: `${LIB_DIR}/security/index.js`,
-    format: 'umd',
-    output: 'security.umd.js',
-    dest: `${LIB_DIR}/security/bundles`,
-  };
-
-  bundle(config);
+    output: `${name}.umd.js`,
+    dest: `${LIB_DIR}/${name}/bundles`,
+  });
 }
 
 function bundle(config: any) {
