c636bf29458cac1b8fb9158b68f520300beed7a8
Dmitry Nehaychik <4dmitr@gmail.com>
2018-10-17 18:34:32 +0300

    fix(auth): don't import http module Closes #631

--- src/framework/auth/auth.module.ts
+++ src/framework/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Injector, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HttpRequest } from '@angular/common/http';
+import { HttpRequest } from '@angular/common/http';
 
 import {
   NbAlertModule,
@@ -95,7 +95,6 @@ export function nbNoOpInterceptorFilter(req: HttpRequest<any>): boolean {
     NbButtonModule,
     RouterModule.forChild(routes),
     FormsModule,
-    HttpClientModule,
   ],
   declarations: [
     NbAuthComponent,
