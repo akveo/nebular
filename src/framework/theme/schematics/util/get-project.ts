fea98e651948cb70568cd00c5470d8f644e98e12
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-10-18 12:45:16 +0300

    fix(schematics): build issues

--- src/framework/theme/schematics/util/get-project.ts
+++ src/framework/theme/schematics/util/get-project.ts
@@ -6,17 +6,17 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {WorkspaceSchema, WorkspaceProject} from '@angular-devkit/core/src/workspace';
+import { WorkspaceProject, WorkspaceSchema } from '@angular-devkit/core/src/workspace';
 
 /**
  * Finds the specified project configuration in the workspace. Throws an error if the project
  * couldn't be found.
  */
 export function getProjectFromWorkspace(
-    workspace: WorkspaceSchema,
-    projectName?: string): WorkspaceProject {
+  workspace: WorkspaceSchema,
+  projectName?: string): WorkspaceProject {
 
-  const project = workspace.projects[projectName || workspace.defaultProject!];
+  const project = workspace.projects[projectName || workspace.defaultProject];
 
   if (!project) {
     throw new Error(`Could not find project in workspace: ${projectName}`);
--- src/framework/theme/schematics/util/ng-module-imports.ts
+++ src/framework/theme/schematics/util/ng-module-imports.ts
@@ -6,7 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {Tree} from '@angular-devkit/schematics';
+import { Tree } from '@angular-devkit/schematics';
 import * as ts from 'typescript';
 
 /**
@@ -20,12 +20,12 @@ export function hasNgModuleImport(tree: Tree, modulePath: string, className: str
   }
 
   const parsedFile = ts.createSourceFile(modulePath, moduleFileContent.toString(),
-      ts.ScriptTarget.Latest, true);
+    ts.ScriptTarget.Latest, true);
   let ngModuleMetadata: ts.ObjectLiteralExpression | null = null;
 
   const findModuleDecorator = (node: ts.Node) => {
     if (ts.isDecorator(node) && ts.isCallExpression(node.expression) &&
-        isNgModuleCallExpression(node.expression)) {
+      isNgModuleCallExpression(node.expression)) {
       ngModuleMetadata = node.expression.arguments[0] as ts.ObjectLiteralExpression;
       return;
     }
@@ -39,9 +39,9 @@ export function hasNgModuleImport(tree: Tree, modulePath: string, className: str
     throw new Error(`Could not find NgModule declaration inside: "${modulePath}"`);
   }
 
-  for (let property of ngModuleMetadata!.properties) {
+  for (const property of ngModuleMetadata.properties) {
     if (!ts.isPropertyAssignment(property) || property.name.getText() !== 'imports' ||
-        !ts.isArrayLiteralExpression(property.initializer)) {
+      !ts.isArrayLiteralExpression(property.initializer)) {
       continue;
     }
 
@@ -69,7 +69,7 @@ function resolveIdentifierOfExpression(expression: ts.Expression): ts.Identifier
 /** Whether the specified call expression is referring to a NgModule definition. */
 function isNgModuleCallExpression(callExpression: ts.CallExpression): boolean {
   if (!callExpression.arguments.length ||
-      !ts.isObjectLiteralExpression(callExpression.arguments[0])) {
+    !ts.isObjectLiteralExpression(callExpression.arguments[0])) {
     return false;
   }
 
