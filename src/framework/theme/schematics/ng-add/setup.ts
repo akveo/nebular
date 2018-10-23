4cb7871cf6095edfd26afe41f49739294b82e817
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-10-23 15:45:02 +0300

    chore(schematics): add ng-add/setup description

--- src/framework/theme/schematics/ng-add/setup.ts
+++ src/framework/theme/schematics/ng-add/setup.ts
@@ -13,8 +13,8 @@ import { wrapRootComponentInLayout } from './wrap-in-layout';
 
 
 /**
- * Setting up Nebular for the project by registering required modules, adding Nebular themes and wrapping
- * root component in the Nebular Layout.
+ * Setting up Nebular for the specified project by registering required modules,
+ * adding Nebular themes and wrapping root component in the Nebular Layout.
  * */
 export default function (options: Schema) {
   return chain([
