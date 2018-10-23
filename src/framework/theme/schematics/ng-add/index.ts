a6fdc8c0de9e2bc74fec02fcdcd50ff5ad868e2a
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-10-23 15:39:26 +0300

    chore(schematics): add ng-add/index description

--- src/framework/theme/schematics/ng-add/index.ts
+++ src/framework/theme/schematics/ng-add/index.ts
@@ -16,19 +16,30 @@ import { addPackageToPackageJson, getPackageVersionFromPackageJson } from '../ut
  * */
 export default function (options: Schema): Rule {
   return (host: Tree, context: SchematicContext) => {
-    addPeerDependenciesToPackageJson(host);
-    runNgAddSetupSchematics(context, options);
+    registerPeerDependencies(host);
+    runSetupSchematics(context, options);
   }
 }
 
-function addPeerDependenciesToPackageJson(host: Tree) {
+/**
+ * Add required peer dependencies in package.json if needed.
+ * */
+function registerPeerDependencies(host: Tree) {
   const angularCoreVersion = getPackageVersionFromPackageJson(host, '@angular/core');
 
   addPackageToPackageJson(host, '@angular/cdk', angularCoreVersion);
   addPackageToPackageJson(host, '@angular/animations', angularCoreVersion);
 }
 
-function runNgAddSetupSchematics(context: SchematicContext, options: Schema) {
+/**
+ * Runs `npm install` and after complete runs `setup` schematics.
+ * The rest part of the ng-add schematics uses `@angular/cdk/schematics` utilities.
+ * That's why we have to install `@angular/cdk` package before running Nebular setup in the project.
+ *
+ * The only possibility to run `setup` schematics after `@angular/cdk` installed
+ * is to use context tasks and add `npm install` task as the dependency to `setup` schematics task.
+ * */
+function runSetupSchematics(context: SchematicContext, options: Schema) {
   const installTaskId = context.addTask(new NodePackageInstallTask());
   context.addTask(new RunSchematicTask('setup', options), [installTaskId]);
 }
