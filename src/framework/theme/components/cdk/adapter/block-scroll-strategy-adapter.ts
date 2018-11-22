02684214010ba0d19784a717b225dfdda00cee38
Nikita Poltoratsky <n.poltoratsky@akveo.com>
2018-11-22 18:01:34 +0300

    fix(select): disable layout scrolling

--- src/framework/theme/components/cdk/adapter/block-scroll-strategy-adapter.ts
+++ src/framework/theme/components/cdk/adapter/block-scroll-strategy-adapter.ts
@@ -1,11 +1,42 @@
-import { Injectable, Inject } from '@angular/core';
-import { BlockScrollStrategy } from '@angular/cdk/overlay';
+import { Inject, Injectable, NgZone } from '@angular/core';
+import { BlockScrollStrategy, ScrollDispatcher, ScrollStrategyOptions } from '@angular/cdk/overlay';
+
+import { NbLayoutScrollService } from '../../../services/scroll.service';
 import { NB_DOCUMENT } from '../../../theme.options';
 import { NbViewportRulerAdapter } from './viewport-ruler-adapter';
 
+
+/**
+ * Overrides default block scroll strategy because default strategy blocks scrolling on the body only.
+ * But Nebular has its own scrollable container - nb-layout. So, we need to block scrolling in it to.
+ * */
 @Injectable()
 export class NbBlockScrollStrategyAdapter extends BlockScrollStrategy {
-  constructor(ruler: NbViewportRulerAdapter, @Inject(NB_DOCUMENT) document) {
-    super(ruler, document);
+  constructor(@Inject(NB_DOCUMENT) document: any,
+              viewportRuler: NbViewportRulerAdapter,
+              protected scrollService: NbLayoutScrollService) {
+    super(viewportRuler, document);
   }
+
+  enable() {
+    super.enable();
+    this.scrollService.scrollable(false);
+  }
+
+  disable() {
+    super.disable();
+    this.scrollService.scrollable(true);
+  }
+}
+
+export class NbScrollStrategyOptions extends ScrollStrategyOptions {
+  constructor(protected scrollService: NbLayoutScrollService,
+              protected scrollDispatcher: ScrollDispatcher,
+              protected viewportRuler: NbViewportRulerAdapter,
+              protected ngZone: NgZone,
+              @Inject(NB_DOCUMENT) protected document) {
+    super(scrollDispatcher, viewportRuler, ngZone, document);
+  }
+
+  block = () => new NbBlockScrollStrategyAdapter(this.document, this.viewportRuler, this.scrollService);
 }
