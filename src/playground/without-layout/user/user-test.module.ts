f1bebe6825c0d004f721cf59804b9ddaddf8f833
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2018-12-17 12:10:13 +0300

    feat: add user test module
    To prevent routes duplication for layout and no-layout dirs

---
+++ src/playground/without-layout/user/user-test.module.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+import { NgModule } from '@angular/core';
+import { UserTestRoutingModule } from './user-test-routing.module';
+import { UserTestComponent } from './user-test.component';
+
+@NgModule({
+  declarations: [ UserTestComponent ],
+  imports: [ UserTestRoutingModule ],
+})
+export class UserTestModule {}
