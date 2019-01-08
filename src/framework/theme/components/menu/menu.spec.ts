6a0b0558e546f437fe2008bc66c0ed3458ef3a0a
u.ahmetvaliyev <ahmetvaliev.vladislav@gmail.com>
2019-01-08 13:10:48 +0300

    docs(chevron): tests moved, chevron style moved

--- src/framework/theme/components/menu/menu.spec.ts
+++ src/framework/theme/components/menu/menu.spec.ts
@@ -62,36 +62,35 @@ describe('menu helpers', () => {
     fixture.detectChanges();
     expect(chevron.classList).toContain('nb-arrow-left');
   });
-
-  describe('menu URL helpers', () => {
-
-    it('isUrlPathContain should work by url segments', () => {
-      expect(isUrlPathContain('/a/ba', '/a/b')).toBeFalsy();
-      expect(isUrlPathContain('/a/b/c', '/a/b')).toBeTruthy();
-    });
-
-    it('isUrlPathContain should work for url with fragments', () => {
-      expect(isUrlPathContain('/a/b#fragment', '/a/b')).toBeTruthy();
-    });
-
-    it('isUrlPathContain should work for url with query strings', () => {
-      expect(isUrlPathContain('/a/b?a=1;b=2&c=3', '/a/b')).toBeTruthy();
-    });
-
-    it('isUrlPathEqual should work for identical paths', () => {
-      expect(isUrlPathEqual('/a/b/c', '/a/b')).toBeFalsy();
-      expect(isUrlPathEqual('/a/b/c', '/a/b/c')).toBeTruthy();
-    });
-
-    it('isUrlPathEqual should work for url with fragments', () => {
-      expect(isUrlPathEqual('/a/b/c#fragment', '/a/b/c')).toBeTruthy();
-    });
-
-    it('isUrlPathEqual should work for url with query strings', () => {
-      expect(isUrlPathEqual('/a/b/c?a=1;b=2&c=3', '/a/b/c')).toBeTruthy();
-    });
-
-  })
 });
 
+describe('menu URL helpers', () => {
+
+  it('isUrlPathContain should work by url segments', () => {
+    expect(isUrlPathContain('/a/ba', '/a/b')).toBeFalsy();
+    expect(isUrlPathContain('/a/b/c', '/a/b')).toBeTruthy();
+  });
+
+  it('isUrlPathContain should work for url with fragments', () => {
+    expect(isUrlPathContain('/a/b#fragment', '/a/b')).toBeTruthy();
+  });
+
+  it('isUrlPathContain should work for url with query strings', () => {
+    expect(isUrlPathContain('/a/b?a=1;b=2&c=3', '/a/b')).toBeTruthy();
+  });
+
+  it('isUrlPathEqual should work for identical paths', () => {
+    expect(isUrlPathEqual('/a/b/c', '/a/b')).toBeFalsy();
+    expect(isUrlPathEqual('/a/b/c', '/a/b/c')).toBeTruthy();
+  });
+
+  it('isUrlPathEqual should work for url with fragments', () => {
+    expect(isUrlPathEqual('/a/b/c#fragment', '/a/b/c')).toBeTruthy();
+  });
+
+  it('isUrlPathEqual should work for url with query strings', () => {
+    expect(isUrlPathEqual('/a/b/c?a=1;b=2&c=3', '/a/b/c')).toBeTruthy();
+  });
+
+})
 
