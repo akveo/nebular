ef331e393ef46c53a530233578d096964be409fe
u.ahmetvaliyev <ahmetvaliev.vladislav@gmail.com>
2019-01-14 17:33:31 +0300

    test(menu): add menu API tests

--- src/framework/theme/components/menu/menu.spec.ts
+++ src/framework/theme/components/menu/menu.spec.ts
@@ -3,11 +3,257 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-
+import { Component, Input } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TestBed } from '@angular/core/testing';
+import { NbMenuModule } from './menu.module';
+import { NbMenuBag, NbMenuItem, NbMenuService } from './menu.service';
+import { NbThemeModule } from '../../theme.module';
 import { isUrlPathContain, isUrlPathEqual } from './url-matching-helpers';
+import { pairwise, take } from 'rxjs/operators';
 
+
+let menuService;
+
+@Component({
+  template: `<nb-menu [items]="items" tag="menu"></nb-menu>`,
+})
+export class MenuTestComponent {
+  constructor (public menuPublicService: NbMenuService) {}
+  @Input() items: NbMenuItem[];
+}
+
+function createTestComponent(menuItems) {
+  TestBed.configureTestingModule({
+    imports: [
+      NbThemeModule.forRoot(),
+      NbMenuModule.forRoot(),
+      RouterTestingModule.withRoutes([]),
+      NoopAnimationsModule,
+    ],
+    declarations: [ MenuTestComponent ],
+    providers: [ NbMenuService ],
+  });
+  const fixture = TestBed.createComponent( MenuTestComponent );
+  fixture.componentInstance.items = menuItems;
+  menuService = fixture.componentInstance.menuPublicService;
+  fixture.detectChanges();
+  return fixture;
+}
+
+describe('NbMenuItem', () => {
+
+  it('should set icon to menu item', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Home',
+        icon: 'test-icon',
+      },
+    ]);
+    const iconWrapper = fixture.nativeElement.querySelector('.menu-icon');
+    expect(iconWrapper.classList).toContain('test-icon');
+  });
+
+  it('should set title to menu item', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Test title',
+      },
+    ]);
+    const titleWrapper = fixture.nativeElement.querySelector('.menu-title').innerHTML;
+    expect(titleWrapper).toEqual('Test title');
+  });
+
+  it('should set link target to menu item', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Link with _blank target',
+        target: '_blank',
+      },
+      {
+        title: 'Link with _self target',
+        target: '_self',
+      },
+      {
+        title: 'Link with any not valid target',
+        target: 'anyNotValid',
+      },
+    ]);
+    const menuLinks = fixture.nativeElement.querySelectorAll('a');
+    expect(menuLinks[0].getAttribute('target')).toEqual('_blank');
+    expect(menuLinks[1].getAttribute('target')).toEqual('_self');
+    expect(menuLinks[2].getAttribute('target')).toEqual('anyNotValid');
+  });
+
+  it('group element should have only span, without link', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Group item',
+        group: true,
+      },
+    ]);
+    const menuItem = fixture.nativeElement.querySelector('.menu-item');
+    expect(menuItem.querySelector('a')).toBeNull();
+    expect(menuItem.querySelector('span')).not.toBeNull();
+  });
+
+  it('hidden element should not be rendered', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Visible item',
+      },
+      {
+        title: 'Hidden item',
+        hidden: true,
+      },
+      {
+        title: 'Visible item',
+      },
+    ]);
+    const menuList = fixture.nativeElement.querySelectorAll('.menu-item');
+    expect(menuList.length).toEqual(2);
+  });
+
+  it('should set child menu items', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Parent item',
+        expanded: true,
+        children: [
+          {
+            title: 'Child item',
+          },
+        ],
+      },
+    ]);
+    const childList = fixture.nativeElement.querySelector('.menu-item > ul.menu-items');
+    expect(childList.classList).toContain('expanded');
+  });
+
+  it('should expand child menu items', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Parent item',
+        expanded: true,
+        children: [
+          {
+            title: 'Child item',
+          },
+        ],
+      },
+    ]);
+    const parentItem = fixture.nativeElement.querySelector('.menu-item');
+    expect(parentItem.querySelector('ul.menu-items')).not.toBeNull();
+  });
+
+  it('should set URL', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Menu Item with link',
+        url: 'https://test.link',
+      },
+    ]);
+    const menuItem = fixture.nativeElement.querySelector('.menu-item');
+    expect(menuItem.querySelector('a').getAttribute('href')).toEqual('https://test.link');
+  });
+
+  it('should set selected item', () => {
+    const selectedItem = {
+      title: 'Menu item selected',
+      selected: true,
+    };
+    const fixture = createTestComponent([
+      {
+        title: 'Menu item not selected',
+      },
+      selectedItem,
+    ]);
+    const activeItem = fixture.nativeElement.querySelector('a.active');
+    expect(activeItem.querySelector('span').innerHTML).toEqual(selectedItem.title);
+  });
+
+});
+
+describe('menu services', () => {
+
+  it('addItems() should add new item to DOM', () => {
+    const fixture = createTestComponent([
+      {
+        title: 'Existing item',
+      },
+    ]);
+    const addItemObject = {
+      title: 'Added item',
+    };
+    const menuListOnInit = fixture.nativeElement.querySelectorAll('li').length;
+    menuService.addItems([addItemObject], 'menu');
+    fixture.detectChanges();
+    const menuListItemAdded = fixture.nativeElement.querySelectorAll('li').length;
+    expect(menuListItemAdded).toBeGreaterThan(menuListOnInit);
+  });
+
+  it('getSelectedItem() should get selected menu item', (done) => {
+    const selectedItem = {
+      title: 'Menu item selected',
+      selected: true,
+    };
+    createTestComponent([
+      {
+        title: 'Menu item not selected',
+      },
+      selectedItem,
+    ]);
+    menuService.getSelectedItem('menu').subscribe((menuBag: NbMenuBag) => {
+      expect(menuBag.item.title).toEqual(selectedItem.title);
+      done();
+    });
+  }, 1000);
+
+  it('collapseAll() should hide all expanded items', (done) => {
+    const fixture = createTestComponent([
+      {
+        title: 'Menu item collapsed',
+        children: [
+          {
+            title: 'Menu item inner',
+          },
+        ],
+      },
+      {
+        title: 'Menu item expanded 1',
+        expanded: true,
+        children: [
+          {
+            title: 'Menu item inner',
+          },
+        ],
+      },
+      {
+        title: 'Menu item expanded 2',
+        expanded: true,
+        children: [
+          {
+            title: 'Menu item inner',
+          },
+        ],
+      },
+    ]);
+    menuService.onSubmenuToggle()
+      .pipe(pairwise(), take(1))
+      .subscribe(([menuBagFirstCollapsed, menuBagSecondCollapsed]: NbMenuBag[]) => {
+        expect(menuBagFirstCollapsed.item.title).toEqual('Menu item expanded 1');
+        expect(menuBagSecondCollapsed.item.title).toEqual('Menu item expanded 2');
+        done();
+      });
+    menuService.collapseAll();
+    fixture.detectChanges();
+  }, 1000);
+
+});
 
 describe('menu URL helpers', () => {
+
   it('isUrlPathContain should work by url segments', () => {
     expect(isUrlPathContain('/a/ba', '/a/b')).toBeFalsy();
     expect(isUrlPathContain('/a/b/c', '/a/b')).toBeTruthy();
@@ -34,6 +280,4 @@ describe('menu URL helpers', () => {
     expect(isUrlPathEqual('/a/b/c?a=1;b=2&c=3', '/a/b/c')).toBeTruthy();
   });
 
-})
-
-
+});
