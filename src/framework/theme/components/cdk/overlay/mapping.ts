c71318b88502565c13576048bfbc387e2ac7c95c
Dmitry Nehaychik <4dmitr@gmail.com>
2019-01-15 12:52:57 +0300

    refactor(overlay): remove unnecessary code

--- src/framework/theme/components/cdk/overlay/mapping.ts
+++ src/framework/theme/components/cdk/overlay/mapping.ts
@@ -1,13 +1,8 @@
 import {
-  ApplicationRef,
-  ComponentFactoryResolver,
   Directive,
-  Inject,
   Injectable,
-  Injector,
   ModuleWithProviders,
   NgModule,
-  NgZone,
   TemplateRef,
   ViewContainerRef,
 } from '@angular/core';
@@ -15,7 +10,6 @@ import {
   CdkPortal,
   CdkPortalOutlet,
   ComponentPortal,
-  DomPortalOutlet,
   Portal,
   PortalInjector,
   PortalModule,
@@ -30,7 +24,6 @@ import {
   Overlay,
   OverlayConfig,
   OverlayContainer,
-  OverlayKeyboardDispatcher,
   OverlayModule,
   OverlayPositionBuilder,
   OverlayRef,
@@ -39,9 +32,6 @@ import {
   ScrollStrategyOptions,
 } from '@angular/cdk/overlay';
 import { Platform } from '@angular/cdk/platform';
-import { Directionality } from '@angular/cdk/bidi';
-
-import { NB_DOCUMENT } from '../../../theme.options';
 
 
 @Directive({ selector: '[nbPortal]' })
@@ -55,94 +45,8 @@ export class NbPortalOutletDirective extends CdkPortalOutlet {
 export class NbComponentPortal<T = any> extends ComponentPortal<T> {
 }
 
-export class NbDomPortalOutlet extends DomPortalOutlet {
-}
-
 @Injectable()
 export class NbOverlay extends Overlay {
-  protected appRef: ApplicationRef;
-  protected static nextUniqueId = 0;
-
-  constructor(
-    /** Scrolling strategies that can be used when creating an overlay. */
-    public scrollStrategies: ScrollStrategyOptions,
-    protected overlayContainer: OverlayContainer,
-    protected componentFactoryResolver: ComponentFactoryResolver,
-    protected positionBuilder: OverlayPositionBuilder,
-    protected keyboardDispatcher: OverlayKeyboardDispatcher,
-    protected injector: Injector,
-    protected ngZone: NgZone,
-    @Inject(NB_DOCUMENT) private document: any,
-    private directionality: Directionality) {
-    super(
-      scrollStrategies,
-      overlayContainer,
-      componentFactoryResolver,
-      positionBuilder,
-      keyboardDispatcher,
-      injector,
-      ngZone,
-      document,
-      directionality,
-    );
-  }
-
-  /**
-   * Creates an overlay.
-   * @param config Configuration applied to the overlay.
-   * @returns Reference to the created overlay.
-   */
-  create(config?: OverlayConfig): OverlayRef {
-    const host = this.createHostElement();
-    const pane = this.createPaneElement(host);
-    const portalOutlet = this.createPortalOutlet(pane);
-    const overlayConfig = new OverlayConfig(config);
-
-    overlayConfig.direction = overlayConfig.direction || this.directionality.value;
-
-    return new OverlayRef(portalOutlet, host, pane, overlayConfig, this.ngZone,
-      this.keyboardDispatcher, this.document);
-  }
-
-  /**
-   * Creates the DOM element for an overlay and appends it to the overlay container.
-   * @returns Newly-created pane element
-   */
-  protected createPaneElement(host: HTMLElement): HTMLElement {
-    const pane = this.document.createElement('div');
-
-    pane.id = `cdk-overlay-${NbOverlay.nextUniqueId++}`;
-    pane.classList.add('cdk-overlay-pane');
-    host.appendChild(pane);
-
-    return pane;
-  }
-
-  /**
-   * Creates the host element that wraps around an overlay
-   * and can be used for advanced positioning.
-   * @returns Newly-create host element.
-   */
-  protected createHostElement(): HTMLElement {
-    const host = this.document.createElement('div');
-    this.overlayContainer.getContainerElement().appendChild(host);
-    return host;
-  }
-
-  /**
-   * Create a DomPortalOutlet into which the overlay content can be loaded.
-   * @param pane The DOM element to turn into a portal outlet.
-   * @returns A portal outlet for the given DOM element.
-   */
-  protected createPortalOutlet(pane: HTMLElement): NbDomPortalOutlet {
-    // We have to resolve the ApplicationRef later in order to allow people
-    // to use overlay-based providers during app initialization.
-    if (!this.appRef) {
-      this.appRef = this.injector.get<ApplicationRef>(ApplicationRef);
-    }
-
-    return new NbDomPortalOutlet(pane, this.componentFactoryResolver, this.appRef, this.injector);
-  }
 }
 
 @Injectable()
