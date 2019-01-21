c3db48c8523beaa4c2564774bc38f86a3e1cba25
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-01-21 14:25:12 +0300

    fix(date-fns date service): use date-fns format function

--- src/framework/date-fns/services/date-fns-date.service.ts
+++ src/framework/date-fns/services/date-fns-date.service.ts
@@ -10,16 +10,23 @@ import { NbNativeDateService } from '@nebular/theme';
 
 import * as dateFnsParse from 'date-fns/parse';
 // @ts-ignore
-import { default as rollupParse} from 'date-fns/parse';
+import { default as rollupParse } from 'date-fns/parse';
+import * as dateFnsFormat from 'date-fns/format';
+// @ts-ignore
+import { default as rollupFormat } from 'date-fns/format';
 
 const parse = rollupParse || dateFnsParse;
-
+const formatDate = rollupFormat || dateFnsFormat;
 
 @Injectable()
 export class NbDateFnsDateService extends NbNativeDateService {
   constructor(@Inject(LOCALE_ID) locale: string) {
     super(locale);
     this.setLocale(locale);
+  }
+
+  format(date: Date, format: string): string {
+    return formatDate(date, format);
   }
 
   parse(date: string, format: string): Date {
