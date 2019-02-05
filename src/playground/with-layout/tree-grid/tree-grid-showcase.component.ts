f730e3b3a9706ba0d3084b1e6841ee659b5caee9
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-02-05 13:25:09 +0300

    docs(treegrid): use row toggle in showcase

--- src/playground/with-layout/tree-grid/tree-grid-showcase.component.ts
+++ src/playground/with-layout/tree-grid/tree-grid-showcase.component.ts
@@ -5,12 +5,7 @@
  */
 
 import { Component } from '@angular/core';
-import {
-  NbTreeGridPresentationNode,
-  NbTreeGridDataSource,
-  NbTreeGridDataSourceBuilder,
-  NbTreeGridNode,
-} from '@nebular/theme';
+import { NbTreeGridDataSource, NbTreeGridDataSourceBuilder, NbTreeGridNode } from '@nebular/theme';
 
 export interface PeriodicElement {
   name: string;
@@ -23,41 +18,24 @@ export interface PeriodicElement {
 const DATA: NbTreeGridNode<PeriodicElement>[] = [
   {
     data: { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    children: [
-      { data: { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' } },
-      {
-        data: { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-        children: [
-          { data: { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' } },
-          { data: { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' } },
-          { data: { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' } },
-          { data: { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' } },
-          {
-            data: { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-            children: [
-              { data: { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' } },
-              { data: { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' } },
-              { data: { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' } },
-              { data: { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' } },
-            ],
-          },
-        ],
-      },
-      { data: { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' } },
-    ],
   },
   {
     data: { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    expanded: true,
     children: [
-      { data: { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' } },
-      { data: { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' } },
+      {
+        data: { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+        children: [
+          { data: { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' } },
+          { data: { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' } },
+        ],
+      },
     ],
   },
   {
-    data: { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    data: { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
     children: [
-      { data: { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' } },
+      { data: { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' } },
+      { data: { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' } },
     ],
   },
 ];
@@ -66,12 +44,10 @@ const DATA: NbTreeGridNode<PeriodicElement>[] = [
   selector: 'nb-tree-grid-showcase',
   templateUrl: './tree-grid-showcase.component.html',
   styles: [`
-    table {
-      width: 100%;
-    }
-
-    tr:hover {
-      background-color: red;
+    button[nbTreeGridRowToggle] {
+      background: transparent;
+      border: none;
+      padding: 0;
     }
   `],
 })
@@ -86,18 +62,5 @@ export class TreeGridShowcaseComponent {
 
   constructor(private dataSourceBuilder: NbTreeGridDataSourceBuilder<PeriodicElement>) {
     this.dataSource = this.dataSourceBuilder.create(DATA);
-  }
-
-  getNameColumnText(row: NbTreeGridPresentationNode<PeriodicElement>): string {
-    const childrenCountText = row.hasChildren() ? `(${this.countChildren(row.children)})` : '';
-    return `${row.data.name} ${childrenCountText}`;
-  }
-
-  private countChildren(nodes: NbTreeGridPresentationNode<PeriodicElement>[]): number {
-    if (!nodes) {
-      return 0;
-    }
-
-    return nodes.reduce((count, n) => count + 1 + this.countChildren(n.children), 0);
   }
 }
