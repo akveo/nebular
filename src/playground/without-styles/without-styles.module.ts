989ee3be85d1535a14b2b2dc3a953e7f8cd85f1c
Dmitry Nehaychik <4dmitr@gmail.com>
2019-03-01 20:05:14 +0300

    feat(playground): add without-styles folder for bootstrap like use-cases fix bootstrap usecases

---
+++ src/playground/without-styles/without-styles.module.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+import { NgModule } from '@angular/core';
+import { WithoutStylesRoutingModule } from './without-styles-routing.module';
+import { WithoutStylesComponent } from './without-styles.component';
+
+@NgModule({
+  declarations: [
+    WithoutStylesComponent,
+  ],
+  imports: [ WithoutStylesRoutingModule ],
+})
+export class WithoutStylesModule {}
