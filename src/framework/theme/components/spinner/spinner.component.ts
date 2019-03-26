0f8b90efb7c5a88bf68e3e10a5458a27bd62b088
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-03-26 23:57:09 +0300

    docs(spinner): add styles section

--- src/framework/theme/components/spinner/spinner.component.ts
+++ src/framework/theme/components/spinner/spinner.component.ts
@@ -11,6 +11,36 @@ import { NbSpinnerSize } from './spinner-size';
 
 /**
  * Styled spinner component
+ *
+ * @styles
+ *
+ * spinner-background-color:
+ * spinner-circle-filled-color:
+ * spinner-circle-empty-color:
+ * spinner-text-color:
+ * spinner-text-font-family:
+ * spinner-text-font-size:
+ * spinner-text-font-weight:
+ * spinner-text-line-height:
+ * spinner-primary-circle-filled-color:
+ * spinner-primary-circle-empty-color:
+ * spinner-info-circle-filled-color:
+ * spinner-info-circle-empty-color:
+ * spinner-success-circle-filled-color:
+ * spinner-success-circle-empty-color:
+ * spinner-warning-circle-filled-color:
+ * spinner-warning-circle-empty-color:
+ * spinner-danger-circle-filled-color:
+ * spinner-danger-circle-empty-color:
+ * spinner-white-circle-filled-color:
+ * spinner-white-circle-empty-color:
+ * spinner-height-xxsmall:
+ * spinner-height-xsmall:
+ * spinner-height-small:
+ * spinner-height-medium:
+ * spinner-height-large:
+ * spinner-height-xlarge:
+ * spinner-height-xxlarge:
  */
 @Component({
   selector: 'nb-spinner',
