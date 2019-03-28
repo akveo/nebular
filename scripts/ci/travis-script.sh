21bf0a57542ba4aac89ff80875b5ba6720527a23
Sergey Andrievskiy <yggg@users.noreply.github.com>
2019-03-28 17:17:57 +0300

    chore: add next branch to travis (#1327)

--- scripts/ci/travis-script.sh
+++ scripts/ci/travis-script.sh
@@ -27,11 +27,7 @@ fi
 CURRENT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
 # Get commit diff
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  fileDiff=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
-else
-  fileDiff=$(git diff --name-only $TRAVIS_BRANCH...HEAD)
-fi
+fileDiff=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
 
 # Check if tests can be skipped
 if [[ ${fileDiff} =~ ^(.*\.md\s*)*$ ]]; then
