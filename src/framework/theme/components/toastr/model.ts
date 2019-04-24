ccdaf9bc31409ec3084caf3525a238a9d67ba62c
Sergey Andrievskiy <yggg@users.noreply.github.com>
2019-04-24 13:32:26 +0300

    feat(toastr): Eva style (#1411)
    BREAKING CHANGE:
    
    NbToastStatus enum removed. Toastr now uses common NbComponentstatus type.
    
    NbToastComponent status class names changed to 'status-[status-name]'.
    'default' class removed.
    
    Following theme properties were renamed:
    toastr-bg -> toastr-background
    toastr-fg -> toastr-text-color
    toastr-border -> toastr-border-style, toastr-border-width
    
    'toastr-icon-radius' removed.

--- src/framework/theme/components/toastr/model.ts
+++ src/framework/theme/components/toastr/model.ts
@@ -6,16 +6,6 @@
 
 import { NbToastrConfig } from './toastr-config';
 
-
-export enum NbToastStatus {
-  SUCCESS = 'success',
-  INFO = 'info',
-  WARNING = 'warning',
-  PRIMARY = 'primary',
-  DANGER = 'danger',
-  DEFAULT = 'default',
-}
-
 export class NbToast {
   title: string;
   message: string;
