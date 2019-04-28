30f3e76486892999849ef1b5aed83cc6c488b9dd
Sergey Andrievskiy <yggg@users.noreply.github.com>
2019-04-28 14:14:55 +0300

    docs(layout): update theme properties list (#1428)

--- src/framework/theme/components/layout/layout.component.ts
+++ src/framework/theme/components/layout/layout.component.ts
@@ -252,21 +252,25 @@ export class NbLayoutFooterComponent {
  *
  * @styles
  *
- * layout-font-family
- * layout-font-size
- * layout-line-height
- * layout-fg
- * layout-bg
- * layout-min-height
- * layout-content-width
- * layout-window-mode-min-width
- * layout-window-mode-max-width: window mode only, after this value layout turns into a floating window
- * layout-window-mode-bg: window mode only, background
- * layout-window-mode-padding-top: window mode only, max padding from top
- * layout-window-shadow: window mode shadow
- * layout-padding
- * layout-medium-padding
- * layout-small-padding
+ * layout-background-color:
+ * layout-text-color:
+ * layout-text-font-family:
+ * layout-text-font-size:
+ * layout-text-font-weight:
+ * layout-text-line-height:
+ * layout-min-height:
+ * layout-content-width:
+ * layout-window-mode-min-width:
+ * layout-window-mode-max-width:
+ * layout-window-mode-background-color:
+ * layout-window-mode-padding-top:
+ * layout-window-shadow:
+ * layout-padding:
+ * layout-medium-padding:
+ * layout-small-padding:
+ * layout-scrollbar-background-color:
+ * layout-scrollbar-color:
+ * layout-scrollbar-width:
  */
 @Component({
   selector: 'nb-layout',
