199ca8ed6247424940541c50ce6e60a4c94d0539
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-04-29 15:11:42 +0300

    build: enable diff check

--- scripts/ci/travis-script.sh
+++ scripts/ci/travis-script.sh
@@ -26,6 +26,19 @@ fi
 
 CURRENT_BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
+# Get commit diff
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  fileDiff=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
+else
+  fileDiff=$(git diff --name-only $TRAVIS_BRANCH...HEAD)
+fi
+
+ # Check if tests can be skipped
+if [[ ${fileDiff} =~ ^(.*\.md\s*)*$ ]]; then
+  echo "Skipping tests since only markdown files changed."
+  exit 0
+fi
+
 start_tunnel
 wait_for_tunnel
 
