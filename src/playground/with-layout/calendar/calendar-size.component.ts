76e649d4e1edd64c55417ab14919a95775aa4c68
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-04-30 15:54:14 +0300

    docs(calendar): decrease heading sizes

--- src/playground/with-layout/calendar/calendar-size.component.ts
+++ src/playground/with-layout/calendar/calendar-size.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nb-calendar-size',
   template: `
-    <h1>Selected date: {{ date | date }}</h1>
+    <h1 class="h5">Selected date: {{ date | date }}</h1>
     <nb-calendar [(date)]="date" size="large">
     </nb-calendar>
   `,
--- src/playground/with-layout/calendar/calendar-start-view.component.ts
+++ src/playground/with-layout/calendar/calendar-start-view.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nb-calendar-start-view',
   template: `
-    <h1>Selected date: {{ date | date }}</h1>
+    <h1 class="h5">Selected date: {{ date | date }}</h1>
     <nb-calendar [(date)]="date" startView="month">
     </nb-calendar>
   `,
