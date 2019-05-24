4363cc3f390d1c4f4a81e0e35cad32ebd3e2898d
Dmitry Nehaychik <4dmitr@gmail.com>
2019-05-24 10:38:42 +0300

    fix(theme): finalize themes with design system (#1481)
    BREAKING CHANGES:
    
    The following sass mixins are removed: `nb-radial-gradient`, `nb-right-gradient`.
    Theme variables changes:
    
    **renamed:**
    font-main -> font-family-primary
    font-secondary -> font-family-secondary
    radius -> border-radius
    color-bg -> background-basic-color-1,
    color-bg-active -> background-basic-color-2,
    color-fg -> text-basic-color,
    color-fg-heading -> text-basic-color,
    color-fg-text -> text-hint-color,
    separator -> divider-color
    scrollbar-fg -> scrollbar-color,
    scrollbar-bg -> scrollbar-background-color,
    scrollbar-thumb-radius -> scrollbar-width
    
    **removed:**
    color-fg-highlight: #40dc7e,
    
    font-weight-thin: 200,
    font-weight-light: 300,
    font-weight-normal: 400,
    font-weight-bolder: 500,
    font-weight-bold: 600,
    font-weight-ultra-bold: 800,
    
    base-font-size: 16px,
    
    font-size-xlg: 1.25rem,
    font-size-lg: 1.125rem,
    font-size: 1rem,
    font-size-sm: 0.875rem,
    font-size-xs: 0.75rem,
    
    padding: 1.25rem,
    margin: 1.5rem,
    line-height: 1.25,
    
    separator: #ebeef2,
    social-color-facebook: #3b5998,
    social-color-twitter: #55acee,
    social-color-google: #dd4b39,
    social-color-linkedin: #0177b5,
    social-color-github: #6b6b6b,
    social-color-stackoverflow: #2f96e8,
    social-color-dribble: #f26798,
    social-color-behance: #0093fa,
    
    radial-gradient: none,
    linear-gradient: none,

--- src/app/layout-theme-toggle/layout-theme-toggle.component.ts
+++ src/app/layout-theme-toggle/layout-theme-toggle.component.ts
@@ -5,12 +5,12 @@ import { NbThemeService } from '@nebular/theme';
   selector: 'nb-layout-theme-toggle',
   styleUrls: ['./layout-theme-toggle.component.scss'],
   template: `
-    <label dir="ltr">
+    <div dir="ltr">
       <button (click)="enable('default')">Default</button>
       <button (click)="enable('dark')">Dark</button>
       <button (click)="enable('cosmic')">Cosmic</button>
       <button (click)="enable('corporate')">Corporate</button>
-    </label>
+    </div>
   `,
 })
 export class LayoutThemeToggleComponent {
