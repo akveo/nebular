52ca3d13e98dd41aa4cc591d99d5170ffb6ec0ff
Dmitry Nehaychik <4dmitr@gmail.com>
2019-05-27 14:10:30 +0300

    chore: enable travis e2e, disable sauce labs

--- e2e/component-shared.ts
+++ e2e/component-shared.ts
@@ -1,15 +1,33 @@
 import { hexToRgbA } from './e2e-helper';
 
 export const cardSizes = [
-  { sizeKey: 'xxsmall', height: '96px' },
+  { sizeKey: 'tiny', height: '96px' },
+  { sizeKey: 'small', height: '336px' },
+  { sizeKey: 'meduim', height: '456px' },
+  { sizeKey: 'large', height: '576px' },
+  { sizeKey: 'giant', height: '696px' },
 ];
 
 export const alertSizes = [
-  { sizeKey: 'xxsmall', height: '52px' },
+  { sizeKey: 'tiny', height: '72px' },
+  { sizeKey: 'small', height: '92px' },
+  { sizeKey: 'meduim', height: '112px' },
+  { sizeKey: 'large', height: '132px' },
+  { sizeKey: 'giant', height: '152px' },
 ];
 
 export const colors = [
-  { colorKey: 'primary', color: hexToRgbA('#8a7fff') },
+  { colorKey: 'primary', color: hexToRgbA('#3366ff') },
+  { colorKey: 'success', color: hexToRgbA('#00d68f') },
+  { colorKey: 'info', color: hexToRgbA('#0095ff') },
+  { colorKey: 'warning', color: hexToRgbA('#ffaa00') },
+  { colorKey: 'danger', color: hexToRgbA('#ff3d71') },
 ];
 
-export const chatSizes = cardSizes;
+export const chatSizes = [
+  { sizeKey: 'tiny', height: '216px' },
+  { sizeKey: 'small', height: '336px' },
+  { sizeKey: 'meduim', height: '456px' },
+  { sizeKey: 'large', height: '576px' },
+  { sizeKey: 'giant', height: '696px' },
+];
