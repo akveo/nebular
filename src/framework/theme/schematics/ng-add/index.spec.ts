250be1c0b82cf2fec09d5b457b4ac552da0c91d5
Dmitry Nehaychik <4dmitr@gmail.com>
2019-05-30 14:00:43 +0300

    fix: tests

--- src/framework/theme/schematics/ng-add/index.spec.ts
+++ src/framework/theme/schematics/ng-add/index.spec.ts
@@ -41,12 +41,23 @@ const EXPECTED_STYLES_SCSS = `@import 'themes';
 `;
 
 const EXPECTED_THEME_SCSS = `@import '~@nebular/theme/styles/theming';
-@import '~@nebular/theme/styles/themes/default';
+@import '~@nebular/theme/styles/themes/${themeName}';
 
 $nb-themes: nb-register-theme((
+
   // add your variables here like:
-  // color-bg: #4ca6ff,
-), default, default);
+
+  // color-primary-100: #f2f6ff,
+  // color-primary-200: #d9e4ff,
+  // color-primary-300: #a6c1ff,
+  // color-primary-400: #598bff,
+  // color-primary-500: #3366ff,
+  // color-primary-600: #274bdb,
+  // color-primary-700: #1a34b8,
+  // color-primary-800: #102694,
+  // color-primary-900: #091c7a,
+
+), ${themeName}, ${themeName});
 `;
 
 function createTestWorkspace(runner: SchematicTestRunner, appOptions: Partial<ApplicationOptions> = {}) {
