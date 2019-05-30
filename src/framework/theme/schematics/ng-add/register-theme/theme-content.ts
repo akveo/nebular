c5d15c685a8b5a42399031b0cf90983c49c1edf0
Dmitry Nehaychik <4dmitr@gmail.com>
2019-05-30 14:57:14 +0300

    feat(schematics): add dark theme (#1534)

--- src/framework/theme/schematics/ng-add/register-theme/theme-content.ts
+++ src/framework/theme/schematics/ng-add/register-theme/theme-content.ts
@@ -9,8 +9,19 @@ export function createThemeContent(themeName: string): string {
 @import '~@nebular/theme/styles/themes/${themeName}';
 
 $nb-themes: nb-register-theme((
+
   // add your variables here like:
-  // color-bg: #4ca6ff,
+
+  // color-primary-100: #f2f6ff,
+  // color-primary-200: #d9e4ff,
+  // color-primary-300: #a6c1ff,
+  // color-primary-400: #598bff,
+  // color-primary-500: #3366ff,
+  // color-primary-600: #274bdb,
+  // color-primary-700: #1a34b8,
+  // color-primary-800: #102694,
+  // color-primary-900: #091c7a,
+
 ), ${themeName}, ${themeName});
 `;
 }
