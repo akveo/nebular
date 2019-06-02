4dab819aac7721deaecb79aaf7e04c952eb4b2a3
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-06-02 14:30:48 +0300

    feat(docs): add Eva section

---
+++ docs/app/@theme/components/eva/eva.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngd-eva',
+  styleUrls: ['./eva.components.scss'],
+  templateUrl: './eva.component.html',
+})
+export class NgdEvaComponent {}
