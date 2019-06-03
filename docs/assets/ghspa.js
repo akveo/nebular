8e9399544b28b614077e2dbf5e84f6137fa3bdc5
Sergey Andrievskiy <yggg@users.noreply.github.com>
2019-06-03 14:18:18 +0200

    chore(docs): add 3.5.0 redirect (#1561)

--- docs/assets/ghspa.js
+++ docs/assets/ghspa.js
@@ -20,10 +20,12 @@
 
 ;(function(l, projectPages) {
 
+  const versions = ['3.5.0'];
+
   var paths = l.pathname.split('/');
   var repo = projectPages ? '/' + paths[1] : '';
-  if (paths[2] === 'next') {
-    repo += '/next';
+  if (versions.includes(paths[2])) {
+    repo += '/' + paths[2];
   }
 
   /* redirect all 404 trafic to index.html */
