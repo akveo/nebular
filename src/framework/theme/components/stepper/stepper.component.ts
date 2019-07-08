868b428789bac724a8224b21e56f699b7b3cd6fb
Andrii <andriisiuta@gmail.com>
2019-07-08 17:35:01 +0300

    feat(stepper): change min/max to inc/dec, fix test typo mistake.

--- src/framework/theme/components/stepper/stepper.component.ts
+++ src/framework/theme/components/stepper/stepper.component.ts
@@ -210,7 +210,7 @@ export class NbStepperComponent {
    * */
   next() {
     if (this.selectedIndex !== this.steps.length - 1) {
-      this.selectedIndex = Math.min(this.selectedIndex + 1, this.steps.length - 1);
+      this.selectedIndex++;
       this.stepChanged.emit(this.selectedIndex);
     }
   }
@@ -220,7 +220,7 @@ export class NbStepperComponent {
    * */
   previous() {
     if (this.selectedIndex !== 0) {
-      this.selectedIndex = Math.max(this.selectedIndex - 1, 0);
+      this.selectedIndex--;
       this.stepChanged.emit(this.selectedIndex);
     }
   }
