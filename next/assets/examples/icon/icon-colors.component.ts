a518760130a7320e3f56b7a86b4556d3444e03b3
Travis CI User <travis@example.org>
2019-08-23 12:02:57 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/akveo/nebular/commit/0b969cbe33c3e007ac6e95d13533803802435e72
    Travis build: https://travis-ci.org/akveo/nebular/builds/575790339

---
+++ next/assets/examples/icon/icon-colors.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'nb-icon-colors',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './icon-colors.component.html',
+})
+export class IconColorsComponent {
+}
