b8966382b062333582a2a9da36c31a6638b50d57
Travis CI User <travis@example.org>
2019-08-29 13:22:24 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/akveo/nebular/commit/2f427bea0cc6a65492fd81ca4f049b8d651b4cea
    Travis build: https://travis-ci.org/akveo/nebular/builds/578340146

--- assets/examples/tabset/tabset-icon.component.ts
+++ assets/examples/tabset/tabset-icon.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NbIconConfig } from '@nebular/theme';
 
 @Component({
   selector: 'nb-tabset-icon',
@@ -11,4 +12,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   `],
 })
 export class TabsetIconComponent {
+  bellIconConfig: NbIconConfig = { icon: 'bell-outline', pack: 'eva' };
 }
