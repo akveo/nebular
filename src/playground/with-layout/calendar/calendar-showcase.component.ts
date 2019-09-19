5a49dd5b8d6f3f2cdb4e63c012fe8548f44682bd
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-09-19 13:09:20 +0300

    docs(calendar): revert showcase example changes

--- src/playground/with-layout/calendar/calendar-showcase.component.ts
+++ src/playground/with-layout/calendar/calendar-showcase.component.ts
@@ -9,9 +9,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nb-calendar-showcase',
   template: `
-    <button (click)="toggleSize()">toggle size</button>
-    <br>
-    <button (click)="toggleShowWeeks()">toggle weeks</button>
     <h1 class="h5">Selected date: {{ date | date }}</h1>
     <nb-calendar [(date)]="date">
     </nb-calendar>
@@ -19,20 +16,4 @@ import { Component } from '@angular/core';
 })
 export class CalendarShowcaseComponent {
   date = new Date();
-
-  size = 'medium';
-
-  showWeekNumber = false;
-
-  toggleSize() {
-    if (this.size === 'medium') {
-      this.size = 'large';
-    } else {
-      this.size = 'medium';
-    }
-  }
-
-  toggleShowWeeks() {
-    this.showWeekNumber = !this.showWeekNumber;
-  }
 }
