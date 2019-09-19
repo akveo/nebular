ed67637827bd46093673fd3d7ff594b567b07aed
outstandyy <zinchenko.vladislav.1995@gmail.com>
2019-09-19 11:54:45 +0300

    feat(autocomplete): remove unused OverlayModule

--- src/framework/theme/components/option-list/option-list.module.ts
+++ src/framework/theme/components/option-list/option-list.module.ts
@@ -4,7 +4,6 @@ import { NbOptionComponent } from './option.component';
 import { NbOptionGroupComponent } from './option-group.component';
 import { NbOptionListComponent } from './option-list.component';
 import { NbCheckboxModule } from '../checkbox/checkbox.module';
-import { NbOverlayModule } from '../cdk/overlay/overlay.module';
 
 const NB_OPTION_LIST_COMPONENTS = [
   NbOptionListComponent,
@@ -19,7 +18,6 @@ const NB_OPTION_LIST_COMPONENTS = [
   imports: [
     CommonModule,
     NbCheckboxModule,
-    NbOverlayModule,
   ],
   exports: [
     ...NB_OPTION_LIST_COMPONENTS,
