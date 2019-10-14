a07487d64c6604af511eee0e92a915776d48675a
Sergey Andrievskiy <yggg@users.noreply.github.com>
2019-10-14 11:52:16 +0300

    fix(sidebar): emit expand event when expanded because of menu c… (#1897)

--- src/framework/theme/components/sidebar/sidebar.component.ts
+++ src/framework/theme/components/sidebar/sidebar.component.ts
@@ -349,7 +349,7 @@ export class NbSidebarComponent implements OnChanges, OnInit, OnDestroy {
       const link = this.getMenuLink(event.target);
 
       if (link && link.nextElementSibling && link.nextElementSibling.classList.contains('menu-items')) {
-        this.expand();
+        this.sidebarService.expand(this.tag);
       }
     }
   }
