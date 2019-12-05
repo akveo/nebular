30a4d32b2131150964c5f1e2c33eaaf0d9f08fe1
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-12-05 11:58:31 +0300

    style: remove unused import

--- src/framework/theme/components/select/select.module.ts
+++ src/framework/theme/components/select/select.module.ts
@@ -7,7 +7,7 @@ import { NbCardModule } from '../card/card.module';
 import { NbButtonModule } from '../button/button.module';
 import { NbSelectComponent, NbSelectLabelComponent } from './select.component';
 import { NbOptionListModule } from '../option-list/option-list.module';
-import { NbIconModule } from '../icon/icon.module';import { NbOptionComponent } from '../option-list/option.component';
+import { NbIconModule } from '../icon/icon.module';
 
 const NB_SELECT_COMPONENTS = [
   NbSelectComponent,
