068fe5baf1211b1f34249a662f73cf35509cbe16
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-12-06 21:06:47 +0300

    test(view mode): check content in all view modes

--- src/framework/theme/components/calendar-kit/components/calendar-navigation/calendar-view-mode.component.spec.ts
+++ src/framework/theme/components/calendar-kit/components/calendar-navigation/calendar-view-mode.component.spec.ts
@@ -6,8 +6,11 @@
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NbCalendarViewModeComponent } from './calendar-view-mode.component';
-import { NbCalendarKitModule } from '../../calendar-kit.module';
+import {
+  NbCalendarViewMode,
+  NbCalendarViewModeComponent,
+  NbCalendarKitModule,
+} from '@nebular/theme';
 
 
 describe('Component: NbCalendarViewModeComponent', () => {
@@ -24,19 +27,53 @@ describe('Component: NbCalendarViewModeComponent', () => {
     componentEl = fixture.debugElement.nativeElement;
   });
 
-  it('should render date', () => {
-    component.date = new Date(2018, 6, 23);
-    fixture.detectChanges();
-    expect(componentEl.querySelector('button').textContent).toContain('July 2018');
-  });
-
   it('should render empty button with when null date', () => {
     fixture.detectChanges();
     expect(componentEl.querySelector('button').textContent).toContain('');
   });
 
-  it('should fire click when interior button clicked', () => {
-    component.changeMode.subscribe(e => expect(e).toBeUndefined());
+  it('should render moth with year in date mode', () => {
+    component.date = new Date(2018, 6, 23);
+    fixture.detectChanges();
+    expect(componentEl.querySelector('button').textContent).toContain('July 2018');
+  });
+
+  it('should render year in month mode', () => {
+    component.date = new Date(2018, 6, 23);
+    component.viewMode = NbCalendarViewMode.MONTH;
+    fixture.detectChanges();
+    expect(componentEl.querySelector('button').textContent).toContain('2018');
+  });
+
+  it('should render years range in year mode', () => {
+    component.date = new Date(2018, 6, 23);
+    component.viewMode = NbCalendarViewMode.YEAR;
+    fixture.detectChanges();
+    expect(componentEl.querySelector('button').textContent).toContain('2016 - 2027');
+  });
+
+  it('should has down icon in date mode', () => {
+    component.date = new Date(2018, 6, 23);
+    fixture.detectChanges();
+    expect(component.getIcon()).toBe('chevron-down-outline');
+  });
+
+  it('should has up icon in date mode', () => {
+    component.date = new Date(2018, 6, 23);
+    component.viewMode = NbCalendarViewMode.MONTH;
+    fixture.detectChanges();
+    expect(component.getIcon()).toBe('chevron-up-outline');
+  });
+
+  it('should has up icon in date mode', () => {
+    component.date = new Date(2018, 6, 23);
+    component.viewMode = NbCalendarViewMode.YEAR;
+    fixture.detectChanges();
+    expect(component.getIcon()).toBe('chevron-up-outline');
+  });
+
+  it('should emit change mode when button clicked', (done) => {
+    component.changeMode.subscribe(done);
     componentEl.querySelector('button').dispatchEvent(new Event('click'));
   });
 });
