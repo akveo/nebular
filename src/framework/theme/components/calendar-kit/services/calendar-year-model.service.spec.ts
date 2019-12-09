585df468132d4eb1e226c8efa898346b6a0fbee9
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2019-12-09 15:14:37 +0300

    test(year service): check for zero and negative years

--- src/framework/theme/components/calendar-kit/services/calendar-year-model.service.spec.ts
+++ src/framework/theme/components/calendar-kit/services/calendar-year-model.service.spec.ts
@@ -22,4 +22,30 @@ describe('NbCalendarYearModelService', () => {
     expect(firstYear.getFullYear()).toBe(2016);
     expect(lastYear.getFullYear()).toBe(2027);
   });
+
+  it('should return correct year range for 0 year', () => {
+    const date = new Date();
+    date.setFullYear(0);
+    const viewYears: Date[][] = yearModelService.getViewYears(date);
+
+    const firstYear = viewYears[0][0];
+    const lastRow = viewYears[viewYears.length - 1];
+    const lastYear = lastRow[lastRow.length - 1];
+
+    expect(firstYear.getFullYear()).toBe(0);
+    expect(lastYear.getFullYear()).toBe(11);
+  });
+
+  it('should return correct year range for negative year', () => {
+    const date = new Date();
+    date.setFullYear(-1);
+    const viewYears: Date[][] = yearModelService.getViewYears(date);
+
+    const firstYear = viewYears[0][0];
+    const lastRow = viewYears[viewYears.length - 1];
+    const lastYear = lastRow[lastRow.length - 1];
+
+    expect(firstYear.getFullYear()).toBe(-12);
+    expect(lastYear.getFullYear()).toBe(-1);
+  });
 });
