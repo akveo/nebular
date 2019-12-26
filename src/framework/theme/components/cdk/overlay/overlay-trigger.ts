ac073af34b71fec2638abaca90968604134a35ac
Sergey Andrievskiy <yggg@users.noreply.github.com>
2019-12-26 12:24:06 +0300

    fix(trigger): prevent stream destroy on mouse leave (#2141)
    (cherry picked from commit 7c79d1d03d0c106edfb8d52f852c71642bf2aa2b)

--- src/framework/theme/components/cdk/overlay/overlay-trigger.ts
+++ src/framework/theme/components/cdk/overlay/overlay-trigger.ts
@@ -102,14 +102,13 @@ export class NbHoverTriggerStrategy extends NbTriggerStrategyBase {
   show$: Observable<Event> = observableFromEvent<Event>(this.host, 'mouseenter')
     .pipe(
       filter(() => !this.container()),
+      // this `delay & takeUntil & repeat` operators combination is a synonym for `conditional debounce`
+      // meaning that if one event occurs in some time after the initial one we won't react to it
       delay(100),
+      // tslint:disable-next-line:rxjs-no-unsafe-takeuntil
+      takeUntil(observableFromEvent(this.host, 'mouseleave')),
       repeat(),
-      takeUntil(
-        observableMerge(
-          observableFromEvent(this.host, 'mouseleave'),
-          this.destroyed$,
-        ),
-      ),
+      takeUntil(this.destroyed$),
     );
 
   hide$: Observable<Event> = observableFromEvent<Event>(this.host, 'mouseleave')
@@ -133,16 +132,13 @@ export class NbHoverTriggerStrategy extends NbTriggerStrategyBase {
 export class NbHintTriggerStrategy extends NbTriggerStrategyBase {
   show$: Observable<Event> = observableFromEvent<Event>(this.host, 'mouseenter')
     .pipe(
-      delay(100),
       // this `delay & takeUntil & repeat` operators combination is a synonym for `conditional debounce`
       // meaning that if one event occurs in some time after the initial one we won't react to it
+      delay(100),
+      // tslint:disable-next-line:rxjs-no-unsafe-takeuntil
+      takeUntil(observableFromEvent(this.host, 'mouseleave')),
       repeat(),
-      takeUntil(
-        observableMerge(
-          observableFromEvent(this.host, 'mouseleave'),
-          this.destroyed$,
-        ),
-      ),
+      takeUntil(this.destroyed$),
     );
 
   hide$: Observable<Event> = observableFromEvent(this.host, 'mouseleave')
