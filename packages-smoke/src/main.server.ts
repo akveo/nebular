bc620f561c5f5362d127a5410ba14c64799e4697
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2020-02-24 10:51:35 +0300

    feat(packages smoke): update to Angular 9

--- packages-smoke/src/main.server.ts
+++ packages-smoke/src/main.server.ts
@@ -10,3 +10,5 @@ export { AppServerModule } from './app/app.server.module';
 export { ngExpressEngine } from "@nguniversal/express-engine";
 export { provideModuleMap } from "@nguniversal/module-map-ngfactory-loader";
 
+
+export { renderModule, renderModuleFactory } from '@angular/platform-server';
