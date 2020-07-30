d519eff641c6080c32bc9bd31a0c0d27d0452acc
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2020-07-30 10:22:01 +0300

    refactor(playground): remove moment adapter

--- src/playground/with-layout/timepicker/timepicker.module.ts
+++ src/playground/with-layout/timepicker/timepicker.module.ts
@@ -5,14 +5,14 @@
  */
 
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NbCardModule, NbTimepickerModule, NbInputModule, NbIconModule, NbFormFieldModule } from '@nebular/theme';
+
 import { TimepickerShowcaseComponent } from './timepicker-showcase.component';
 import { TimepickerTwelveHoursFormatComponent } from './timepicker-twelve-hours-format.component';
 import { TimepickerSingleColumnComponent } from './timepicker-single-column.component';
 import { TimepickerWithSecondsComponent } from './timepicker-with-seconds.component';
 import { TimepickerRoutingModule } from './timepicker-routing.module';
-import { NbCardModule, NbTimepickerModule, NbInputModule, NbIconModule, NbFormFieldModule } from '@nebular/theme';
-import { CommonModule } from '@angular/common';
-import { NbMomentDateModule } from '../../../framework/moment/moment.module';
 
 @NgModule({
   declarations: [
@@ -22,14 +22,13 @@ import { NbMomentDateModule } from '../../../framework/moment/moment.module';
     TimepickerWithSecondsComponent,
   ],
   imports: [
-    NbTimepickerModule.forRoot({isTwelveHoursFormat: false}),
+    NbTimepickerModule.forRoot({ isTwelveHoursFormat: false }),
     TimepickerRoutingModule,
     NbInputModule,
     NbCardModule,
     NbIconModule,
     NbFormFieldModule,
     CommonModule,
-    NbMomentDateModule,
   ],
 })
 export class TimepickerModule {}
