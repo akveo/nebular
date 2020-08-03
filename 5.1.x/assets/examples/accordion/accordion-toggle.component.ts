a46419dd7480409cb71bb07caba7b32f2ce02944
Sergey Andrievskiy <st.17028@gmail.com>
2020-08-03 20:26:51 +0300

    Auto-generated commit

---
+++ 5.1.x/assets/examples/accordion/accordion-toggle.component.ts
@@ -0,0 +1,16 @@
+import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'nb-accordion-toggle',
+  templateUrl: './accordion-toggle.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'example-height-60' },
+})
+export class AccordionToggleComponent {
+
+  @ViewChild('item') accordion;
+
+  toggle() {
+    this.accordion.toggle();
+  }
+}
---
+++ 5.1.x/assets/examples/firebase/firebase-playground.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nb-firebase-playground',
+  template: `
+    <router-outlet></router-outlet>
+  `,
+})
+export class FirebasePlaygroundComponent {
+}
