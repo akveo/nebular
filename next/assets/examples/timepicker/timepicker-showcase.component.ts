c46a6952001496386a82bf1bf12f19c61d2eef23
Travis CI User <travis@example.org>
2020-09-10 08:07:16 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/akveo/nebular/commit/70ee391be03f8bb188768e1bedba8ce41e96f72a
    Travis build: https://travis-ci.org/akveo/nebular/builds/725839642

---
+++ next/assets/examples/timepicker/timepicker-showcase.component.ts
@@ -0,0 +1,25 @@
+/*
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'nb-timepicker-showcase',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <nb-card size="large">
+      <nb-card-body>
+        <input nbInput
+               type="text"
+               [nbTimepicker]="timepicker"/>
+        <nb-timepicker #timepicker></nb-timepicker>
+      </nb-card-body>
+    </nb-card>
+  `,
+  styleUrls: ['./timepicker-example.scss'],
+})
+export class TimepickerShowcaseComponent {
+}
