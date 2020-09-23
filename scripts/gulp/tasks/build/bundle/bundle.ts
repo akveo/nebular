e64bac0d787090f61eb7060b107efe1e101a8a62
Sergey Andrievskiy <yggg@users.noreply.github.com>
2020-09-23 16:34:26 +0300

    fix(build): provide ES5 code in UMD bundles (#2520)

--- scripts/gulp/tasks/build/bundle/bundle.ts
+++ scripts/gulp/tasks/build/bundle/bundle.ts
@@ -3,7 +3,7 @@ import * as rollup from 'gulp-rollup';
 import * as rename from 'gulp-rename';
 import * as replace from 'gulp-replace';
 
-import { JS_PACKAGES, LIB_DIR } from '../../config';
+import { ES5_BUILD_DIR, JS_PACKAGES, LIB_DIR } from '../../config';
 import { ROLLUP_COMMON_CONFIG } from './rollup-config';
 
 for (const packageName of JS_PACKAGES) {
@@ -34,16 +34,25 @@ function bundleFesm2015Module(name: string) {
 
 function bundleUmdModule(name: string) {
   return bundle({
-    src: `${LIB_DIR}/${name}/esm2015/**/*.js`,
+    src: `${ES5_BUILD_DIR}/${name}/**/*.js`,
     moduleName: `nb.${name}`,
-    entry: `${LIB_DIR}/${name}/esm2015/index.js`,
+    entry: `${ES5_BUILD_DIR}/${name}/index.js`,
     format: 'umd',
     output: `${name}.umd.js`,
     dest: `${LIB_DIR}/${name}/bundles`,
   });
 }
 
-function bundle(config: any) {
+interface BundleConfig {
+  src: string;
+  moduleName: string;
+  entry: string;
+  format: string;
+  output: string;
+  dest: string;
+}
+
+function bundle(config: BundleConfig) {
   return src(config.src)
     .pipe(rollup(Object.assign({}, ROLLUP_COMMON_CONFIG, {
       moduleName: config.moduleName,
