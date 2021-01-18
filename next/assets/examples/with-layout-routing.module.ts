deab53820636c60c2420efacb2e8c2f3fbce36f4
Travis CI User <travis@example.org>
2021-01-18 10:40:32 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/akveo/nebular/commit/bcdfcd8a75c9f4593ff2c9295f0280de59f8ed4f
    Travis build: https://travis-ci.org/akveo/nebular/builds/213484170

--- next/assets/examples/with-layout-routing.module.ts
+++ next/assets/examples/with-layout-routing.module.ts
@@ -120,6 +120,10 @@ const routes: Route[] = [
         loadChildren: () => import('./stepper/stepper.module').then(m => m.StepperModule),
       },
       {
+        path: 'tag',
+        loadChildren: () => import('./tag/tag.module').then(m => m.TagModule),
+      },
+      {
         path: 'tabset',
         loadChildren: () => import('./tabset/tabset.module').then(m => m.TabsetModule),
       },
