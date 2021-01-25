e5d33af96dafbfc37feb65e9d658e21a8de56d0b
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2021-01-25 15:27:02 +0300

    test(sidebar): add responsive state checks

--- src/framework/theme/components/sidebar/sidebar.spec.ts
+++ src/framework/theme/components/sidebar/sidebar.spec.ts
@@ -1,8 +1,10 @@
-import { Component, DebugElement } from '@angular/core';
+import { Component, DebugElement, Injectable } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable, Subject } from 'rxjs';
+import { pairwise, startWith } from 'rxjs/operators';
 import {
   NbSidebarComponent,
   NbMenuModule,
@@ -11,12 +13,17 @@ import {
   NbMenuComponent,
   NbThemeModule,
   NbMenuItemComponent,
-  NbIconComponent, NbSidebarService,
+  NbIconComponent,
+  NbSidebarService,
+  NbSidebarState,
+  NbMediaBreakpoint,
+  NbThemeService,
+  NbMediaBreakpointsService,
 } from '@nebular/theme';
 
 @Component({
   template: `
-    <nb-sidebar>
+    <nb-sidebar [responsive]="responsive" [state]="state">
       <button id="button-outside-menu"></button>
       <nb-menu [items]="menuItems"></nb-menu>
     </nb-sidebar>
@@ -36,6 +43,32 @@ export class SidebarExpandTestComponent {
       group: true,
     },
   ];
+
+  responsive = false;
+  state: NbSidebarState = 'expanded';
+}
+
+@Injectable()
+export class MockThemeService {
+  private breakpoint$ = new Subject<NbMediaBreakpoint>();
+
+  constructor(private breakpointsService: NbMediaBreakpointsService) {
+  }
+
+  setBreakpointTo(breakpointName: string): void {
+    this.breakpoint$.next(this.breakpointsService.getByName(breakpointName));
+  }
+
+  onMediaQueryChange(): Observable<NbMediaBreakpoint[]> {
+    const breakpoints = this.breakpointsService.getBreakpoints();
+    const largestBreakpoint = breakpoints[breakpoints.length - 1];
+
+    return this.breakpoint$
+      .pipe(
+        startWith({ name: 'unknown', width: undefined }, largestBreakpoint),
+        pairwise(),
+      );
+  }
 }
 
 describe('NbSidebarComponent', () => {
@@ -48,6 +81,10 @@ describe('NbSidebarComponent', () => {
         NbSidebarModule.forRoot(),
         NbMenuModule.forRoot(),
       ],
+      providers: [
+        MockThemeService,
+        { provide: NbThemeService, useExisting: MockThemeService },
+      ],
       declarations: [ SidebarExpandTestComponent ],
     });
   });
@@ -55,12 +92,14 @@ describe('NbSidebarComponent', () => {
   describe('States (expanded, collapsed, compacted)', () => {
     let fixture: ComponentFixture<SidebarExpandTestComponent>;
     let sidebarComponent: NbSidebarComponent;
+    let themeService: MockThemeService;
 
     beforeEach(() => {
       fixture = TestBed.createComponent(SidebarExpandTestComponent);
       fixture.detectChanges();
 
       sidebarComponent = fixture.debugElement.query(By.directive(NbSidebarComponent)).componentInstance;
+      themeService = TestBed.inject(MockThemeService);
     });
 
     it(`should collapse when collapse method called`, () => {
@@ -141,6 +180,49 @@ describe('NbSidebarComponent', () => {
       fixture.detectChanges();
 
       expect(sidebarComponent.compacted).toEqual(true);
+    });
+
+    it('should expand when responsive is set to false', () => {
+      sidebarComponent.responsive = true;
+      sidebarComponent.collapse();
+
+      expect(sidebarComponent.state).toEqual('collapsed');
+
+      sidebarComponent.responsive = false;
+
+      expect(sidebarComponent.state).toEqual('expanded');
+    });
+
+    it('should update state according to the current breakpoint when responsive turns on (update to compacted)', () => {
+      const compactedBreakpoints = sidebarComponent.compactedBreakpoints;
+      const largestCompactedBreakpointName = compactedBreakpoints[compactedBreakpoints.length - 1];
+      themeService.setBreakpointTo(largestCompactedBreakpointName);
+
+      expect(sidebarComponent.state).toEqual('expanded');
+
+      sidebarComponent.responsive = true;
+
+      expect(sidebarComponent.state).toEqual('compacted');
+    });
+
+    it('should update state according to the current breakpoint when responsive turns on (update to collapsed)', () => {
+      themeService.setBreakpointTo(sidebarComponent.collapsedBreakpoints[0]);
+
+      expect(sidebarComponent.state).toEqual('expanded');
+
+      sidebarComponent.responsive = true;
+
+      expect(sidebarComponent.state).toEqual('collapsed');
+    });
+
+    it('should expand when responsive and initial state is in different breakpoint', () => {
+      fixture = TestBed.createComponent(SidebarExpandTestComponent);
+      fixture.componentInstance.responsive = true;
+      fixture.componentInstance.state = 'compacted';
+      fixture.detectChanges();
+      sidebarComponent = fixture.debugElement.query(By.directive(NbSidebarComponent)).componentInstance;
+
+      expect(sidebarComponent.state).toEqual('expanded');
     });
   });
 });
