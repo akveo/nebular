2b1cfcdef092c12655461e56656d90a3f56f0d68
Amaury Lapaque <laam@sbim.be>
2021-03-12 10:35:37 +0100

    fixing tslint things....

--- src/playground/with-layout/window/window.module.ts
+++ src/playground/with-layout/window/window.module.ts
@@ -4,16 +4,16 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-import { CommonModule }                                  from '@angular/common';
-import { NgModule }                                      from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { NbButtonModule, NbInputModule, NbWindowModule } from '@nebular/theme';
-import { WindowNamePromptComponent }                     from './components/name-prompt-window.component';
-import { WindowResultComponent }                         from './window-result.component';
-import { WindowRoutingModule }                           from './window-routing.module';
-import { TemplateWindowComponent }                       from './template-window.component';
-import { WindowShowcaseComponent }                       from './window-showcase.component';
-import { WindowsBackdropComponent }                      from './windows-backdrop.component';
-import { FormComponent }                                 from './components/form.component';
+import { WindowNamePromptComponent } from './components/name-prompt-window.component';
+import { WindowResultComponent } from './window-result.component';
+import { WindowRoutingModule } from './window-routing.module';
+import { TemplateWindowComponent } from './template-window.component';
+import { WindowShowcaseComponent } from './window-showcase.component';
+import { WindowsBackdropComponent } from './windows-backdrop.component';
+import { FormComponent } from './components/form.component';
 
 @NgModule({
             declarations: [
