29cf5397aabd70af01aa01f2f44ec42b1963322d
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2021-06-15 13:44:25 +0300

    refactor(button group): update value change example

--- src/playground/with-layout/button-group/button-group-value-change.component.ts
+++ src/playground/with-layout/button-group/button-group-value-change.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button-group-value-change',
   templateUrl: './button-group-value-change.component.html',
-  styleUrls: ['./button-group-value-change.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ButtonGroupValueChangeComponent {
-  logSingle = '';
-  logMultiple = '';
+  singleSelectGroupValue = [];
+  multiSelectGroupValue = [];
 
-  onValueChangeSingle($event: unknown[]) {
-    this.logSingle = `Current value is ${JSON.stringify($event)}`;
+  constructor(private cd: ChangeDetectorRef) {
   }
 
-  onValueChangeMultiple($event: unknown[]) {
-    this.logMultiple = `Current value is ${JSON.stringify($event)}`;
+  updateSingleSelectGroupValue(value): void {
+    this.singleSelectGroupValue = value;
+    this.cd.markForCheck();
+  }
+
+  updateMultiSelectGroupValue(value): void {
+    this.multiSelectGroupValue = value;
+    this.cd.markForCheck();
   }
 }
