577d06c11782b66b2fa0ad3cc5229076c9c5fc93
kolyaevtikhovich <evtkhvch@gmail.com>
2021-09-02 22:37:35 +0300

    ref(docs): add article service

--- docs/app/blocks/components/md-block/md-block.component.ts
+++ docs/app/blocks/components/md-block/md-block.component.ts
@@ -7,36 +7,40 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { map, takeUntil } from 'rxjs/operators';
-import { NgdTextService } from '../../../@theme/services';
-import { HttpClient } from '@angular/common/http';
+import { NgdArticleService, NgdTextService } from '../../../@theme/services';
 
 @Component({
   selector: 'ngd-md-block',
   template: `
-    <nb-card *ngFor="let section of content;" [ngdFragment]="section?.fragment">
+    <nb-card *ngFor="let section of content;" [ngdFragment]="section.fragment">
       <nb-card-body>
-        <div [innerHtml]="section?.html"></div>
+        <div [innerHtml]="section.html"></div>
       </nb-card-body>
     </nb-card>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NgdMdBLockComponent implements OnInit, OnDestroy {
-  @Input() source: string;
+  @Input() source: string | undefined;
   public content: MdChildren[] = [];
   private _destroy = new Subject();
 
-  constructor(private textService: NgdTextService, private http: HttpClient, private cdr: ChangeDetectorRef) {
+  constructor(
+    private textService: NgdTextService,
+    private articleService: NgdArticleService,
+    private cdr: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
-    this.http.get(`articles/${this.source}`, { responseType: 'text' }).pipe(
-      takeUntil(this._destroy),
-      map((item) => this.textService.mdToSectionsHTML(item)),
-    ).subscribe((item) => {
-      this.content = item;
-      this.cdr.markForCheck();
-    });
+    if (this.source) {
+      this.articleService.getArticle(this.source).pipe(
+        takeUntil(this._destroy),
+        map((item) => this.textService.mdToSectionsHTML(item)),
+      ).subscribe((item) => {
+        this.content = item;
+        this.cdr.markForCheck();
+      });
+    }
   }
 
   ngOnDestroy(): void {
