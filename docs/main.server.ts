62719d7ec38bf1bce73b178425f3ad362398b8e4
evtkhvch <evtkhvch@gmail.com>
2021-11-04 15:47:00 +0300

    docs: remove localize package

--- docs/main.server.ts
+++ docs/main.server.ts
@@ -7,11 +7,6 @@
  */
 import '@angular/platform-server/init';
 
-/***************************************************************************************************
- * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
- */
-import '@angular/localize/init';
-
 import { enableProdMode } from '@angular/core';
 
 import { environment } from './environments/environment';
