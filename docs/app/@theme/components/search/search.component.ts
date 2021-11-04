347197136b6603dd82ad4629c74855655f2ac8bb
evtkhvch <evtkhvch@gmail.com>
2021-11-04 14:54:16 +0300

    style(docs search): fix platform check

--- docs/app/@theme/components/search/search.component.ts
+++ docs/app/@theme/components/search/search.component.ts
@@ -1,22 +1,21 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, Inject, PLATFORM_ID } from '@angular/core';
 import { NB_WINDOW } from '@nebular/theme';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'ngd-search',
   styleUrls: ['./search.component.scss'],
   template: `
     <nb-icon icon="search-outline"></nb-icon>
-    <input type="text" nbInput id="doc-search" placeholder="Start typing...">
+    <input type="text" nbInput id="doc-search" placeholder="Start typing..." />
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NgdSearchComponent implements AfterViewInit {
-
-  constructor(@Inject(NB_WINDOW) private window) {
-  }
+  constructor(@Inject(NB_WINDOW) private window, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit() {
-    if (this.window) {
+    if (isPlatformBrowser(this.platformId)) {
       this.window.docsearch({
         apiKey: 'fe0dbef6ee2b748314266d7d71d7dea3',
         indexName: 'nebular',
