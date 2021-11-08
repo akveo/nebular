cf79be4f55d8a20af46ca63b7a39bd326ca03e17
evtkhvch <evtkhvch@gmail.com>
2021-11-08 17:49:34 +0300

    fix(tabset): remove NB_TAB_LABEL injection token

--- src/framework/theme/components/tabset/tab-label.ts
+++ src/framework/theme/components/tabset/tab-label.ts
@@ -1,12 +1,13 @@
-import { Directive, InjectionToken } from '@angular/core';
-import { NbPortal } from '../cdk/portal';
-
-export const NB_TAB_LABEL = new InjectionToken<NbTabLabelDirective>('NbTabLabel');
+import { Directive, TemplateRef } from '@angular/core';
 
 /** Used to flag tab labels for use with the portal directive */
 @Directive({
   selector: '[nbTabLabel]',
-  providers: [{ provide: NB_TAB_LABEL, useExisting: NbTabLabelDirective }],
 })
-export class NbTabLabelDirective extends NbPortal {
+export class NbTabLabelDirective {
+  constructor(private templateRef: TemplateRef<any>) {}
+
+  get template(): TemplateRef<any> {
+    return this.templateRef;
+  }
 }
