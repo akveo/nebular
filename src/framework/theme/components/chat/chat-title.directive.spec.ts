9d1cef2d2726c96f56e5e0538ee8ce164dc8b8e0
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2021-11-16 18:07:38 +0300

    test(chat title): refine tests

--- src/framework/theme/components/chat/chat-title.directive.spec.ts
+++ src/framework/theme/components/chat/chat-title.directive.spec.ts
@@ -1,18 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { NbThemeModule } from '../../theme.module';
-import { NbChatModule } from './chat.module';
+import { Component } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { NbThemeModule, NbChatModule, NbChatComponent } from '@nebular/theme';
 
 @Component({
-  selector: 'nb-chat-title-template-test',
   template: `
-    <nb-chat size="large" [title]="title">
-      <ng-template nbChatTitle [context]="{ text: 'some text to pass into template' }" let-data>
-        <div #templateTitle class="template-title">
-          Chat title content from template. Here is the text provided via context: "{{ data.text }}"
-        </div>
+    <nb-chat [title]="title">
+      <ng-template nbChatTitle [context]="{ text: contextTemplateText }" let-data>
+        {{ staticTemplateText }} {{ data.text }}
       </ng-template>
 
       <nb-chat-message
@@ -35,69 +31,66 @@ import { NbChatModule } from './chat.module';
   `,
 })
 export class NbChatTitleTemplateTestComponent {
-  @ViewChild('templateTitle') templateTitle: ElementRef;
-
-  title: string;
-
-  messages = [];
-
-  loadMessages(): void {
-    this.messages = [
-      {
-        reply: false,
-        type: 'link',
-        optionalData: {
-          href: 'https://akveo.github.io/ngx-admin/',
-          label: 'Visit Akveo Nebular',
-        },
-        date: new Date(),
-        user: {
-          name: 'Frodo Baggins',
-          avatar: 'https://i.gifer.com/no.gif',
-        },
+  messages = [
+    {
+      reply: false,
+      type: 'link',
+      optionalData: {
+        href: 'https://akveo.github.io/ngx-admin/',
+        label: 'Visit Akveo Nebular',
       },
-      {
-        text: 'Hello, how are you?',
-        reply: true,
-        type: 'text',
-        date: new Date(),
-        user: {
-          name: 'Bilbo Baggins',
-          avatar: '',
-        },
+      date: new Date(),
+      user: {
+        name: 'Frodo Baggins',
+        avatar: 'https://i.gifer.com/no.gif',
       },
-    ];
-  }
+    },
+    {
+      text: 'Hello, how are you?',
+      reply: true,
+      type: 'text',
+      date: new Date(),
+      user: {
+        name: 'Bilbo Baggins',
+        avatar: '',
+      },
+    },
+  ];
+
+  title = 'chat title';
+  staticTemplateText = 'staticTemplateText';
+  contextTemplateText = 'contextTemplateText';
 }
 
-describe('Directive nbChatTitle: NbChatTitleDirective', () => {
+describe('NbChatTitleDirective', () => {
   let fixture: ComponentFixture<NbChatTitleTemplateTestComponent>;
-  let component: NbChatTitleTemplateTestComponent;
+  let testComponent: NbChatTitleTemplateTestComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, NbThemeModule.forRoot(), NbChatModule],
+      imports: [NoopAnimationsModule, NbThemeModule.forRoot(), NbChatModule],
       declarations: [NbChatTitleTemplateTestComponent],
     });
 
     fixture = TestBed.createComponent(NbChatTitleTemplateTestComponent);
-    component = fixture.componentInstance;
+    testComponent = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should render title template if provided', () => {
-    const chatTitleTemplateElement = fixture.nativeElement.querySelector('.template-title');
-    expect(chatTitleTemplateElement).toBeTruthy();
-    expect(chatTitleTemplateElement).toEqual(component.templateTitle.nativeElement);
+    const chatHeaderElement: HTMLElement = fixture.debugElement
+      .query(By.directive(NbChatComponent))
+      .query(By.css('.header')).nativeElement;
+    const expectedText = ` ${testComponent.staticTemplateText} ${testComponent.contextTemplateText} `;
+
+    expect(chatHeaderElement.textContent).toEqual(expectedText);
   });
 
   it('should not render text title if template title is provided', () => {
-    const titleText = 'Some title';
-    component.title = titleText;
-    const chatHeaderElement: HTMLElement = fixture.nativeElement.querySelector('.header');
-    const chatTitleTemplateElement = fixture.nativeElement.querySelector('.template-title');
+    const chatHeaderElement: HTMLElement = fixture.debugElement
+      .query(By.directive(NbChatComponent))
+      .query(By.css('.header')).nativeElement;
 
-    expect(chatHeaderElement.innerText).not.toEqual(titleText);
-    expect(chatTitleTemplateElement).toBeTruthy();
+    expect(chatHeaderElement.textContent).not.toContain(testComponent.title);
   });
 });
