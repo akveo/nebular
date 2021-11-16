f174f3632ecd1716fad1d4499443a10de8f7d9fb
Sergey Andrievskiy <s.andrievskiy@akveo.com>
2021-11-16 19:00:46 +0300

    refactor(theme toggle): align radio circle with text
    And remove redundant rules

--- src/app/layout-theme-toggle/layout-theme-toggle.component.ts
+++ src/app/layout-theme-toggle/layout-theme-toggle.component.ts
@@ -6,15 +6,15 @@ import { NbThemeService } from '@nebular/theme';
   styleUrls: ['./layout-theme-toggle.component.scss'],
   template: `
     <div dir="ltr">
-      <label *ngFor="let theme of themeList; index as i" class="theme-radio">
+      <label *ngFor="let theme of themeList; index as i" class="theme-radio-label">
         <input
           type="radio"
           [value]="theme"
           name="theme"
           [attr.checked]="i === 0 || null"
           (change)="handleChange(theme)"
-        />
-        {{ theme | titlecase }}
+          class="theme-radio"
+        />{{ theme | titlecase }}
       </label>
     </div>
   `,
