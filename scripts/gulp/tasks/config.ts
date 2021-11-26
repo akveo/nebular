0a2d4708a321202d809c5175998d1c96808c620c
Kate <43079840+katebatura@users.noreply.github.com>
2021-11-26 11:50:45 +0300

    fix(8.0.x): change build docs output path (#2962)
    * fix(8.0.x): change build docs output path
    
    * change build docs output path

--- scripts/gulp/tasks/config.ts
+++ scripts/gulp/tasks/config.ts
@@ -4,7 +4,7 @@ export const ES5_BUILD_DIR = './.es5_build';
 export const LIB_DIR = './src/.lib';
 export const DOCS_OUTPUT = './docs/output.json';
 export const EXTENSIONS = ['ts', 'html', 'scss'];
-export const DOCS_DIST = './docs/dist';
+export const DOCS_DIST = './dist/docs';
 export const DEV_SCHEMATICS_PATH = './schematics';
 export const JS_PACKAGES = [
   'auth',
