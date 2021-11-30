a0997d262231d427db12ae5efcf7f8c69f8c4206
Uladzislau Sakalou <sokolov.stb@gmail.com>
2021-11-30 17:35:47 +0300

    docs(toastr): use positions enum (#2956)

--- src/playground/with-layout/toastr/toastr-showcase.component.ts
+++ src/playground/with-layout/toastr/toastr-showcase.component.ts
@@ -1,14 +1,13 @@
 import { Component, HostBinding } from '@angular/core';
-import { NbToastrService } from '@nebular/theme';
+import { NbGlobalPhysicalPosition, NbToastrService } from '@nebular/theme';
 
 @Component({
-  selector: 'nb-toastr-showcase',
   template: `
-    <button nbButton (click)="showToast('top-right', 'success')">Top Right</button>
-    <button nbButton (click)="showToast('bottom-left', 'info')">Bottom left</button>
+    <button nbButton (click)="showToast(positions.TOP_RIGHT, 'success')">Top Right</button>
+    <button nbButton (click)="showToast(positions.BOTTOM_LEFT, 'info')">Bottom left</button>
   `,
   styles: [
-      `
+    `
       ::ng-deep nb-layout-column {
         height: 80vw;
       }
@@ -21,14 +20,12 @@ export class ToastrShowcaseComponent {
   @HostBinding('class')
   classes = 'example-items-rows';
 
-  constructor(private toastrService: NbToastrService) {
-  }
+  positions = NbGlobalPhysicalPosition;
+
+  constructor(private toastrService: NbToastrService) {}
 
   showToast(position, status) {
     this.index += 1;
-    this.toastrService.show(
-      status || 'Success',
-      `Toast ${this.index}`,
-      { position, status });
+    this.toastrService.show(status || 'Success', `Toast ${this.index}`, { position, status });
   }
 }
