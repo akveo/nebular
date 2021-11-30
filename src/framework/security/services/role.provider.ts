d962641612f161d13f6ddde91c86af646efe199a
Uladzislau Sakalou <uladzislausakalou@Uladzislaus-MacBook-Air.local>
2021-11-30 13:03:18 +0300

    style: apply prettier to security module

--- src/framework/security/services/role.provider.ts
+++ src/framework/security/services/role.provider.ts
@@ -6,10 +6,9 @@
 import { Observable } from 'rxjs';
 
 export abstract class NbRoleProvider {
-
   /**
    * Returns current user role
    * @returns {Observable<string | string[]>}
    */
-  abstract getRole(): Observable<string|string[]>;
+  abstract getRole(): Observable<string | string[]>;
 }
