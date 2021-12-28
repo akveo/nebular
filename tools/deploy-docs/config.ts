350b9d006e538ecfab0f026b7e481b4b371aa278
katebatura <katebatura@gmail.com>
2021-12-28 15:38:00 +0300

    ci-deploy

--- tools/deploy-docs/config.ts
+++ tools/deploy-docs/config.ts
@@ -1,2 +1,2 @@
-export const REPO_URL = 'https://github.com/katebatura/nebular.git';
+export const REPO_URL = 'https://github.com/akveo/nebular.git';
 export const OUT_DIR = 'dist/docs'; // Relative to the directory you run command
