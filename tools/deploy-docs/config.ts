7bb1a9d1fec77341d6f6eebc59f0279e70e22d3c
Anderson Feitosa <andersonmfjr@gmail.com>
2022-01-19 16:28:33 -0300

    chore: update config to deploy docs

--- tools/deploy-docs/config.ts
+++ tools/deploy-docs/config.ts
@@ -1,4 +1,4 @@
-export const REPO_URL = 'https://github.com/akveo/nebular.git';
-export const REPO_OWNER = 'akveo';
-export const REPO_NAME = 'nebular';
+export const REPO_URL = 'https://github.com/dadosfera/design-system.git';
+export const REPO_OWNER = 'dadosfera';
+export const REPO_NAME = 'design-system';
 export const OUT_DIR = 'dist/docs'; // Relative to the directory you run command
