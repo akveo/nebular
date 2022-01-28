12a641321705b494e04311c4725eed054f61715d
Anderson Feitosa <andersonmfjr@gmail.com>
2022-01-28 14:18:35 -0300

    feat(pagination): update some responsive styles

--- src/framework/theme/components/pagination/pagination.component.spec.ts
+++ src/framework/theme/components/pagination/pagination.component.spec.ts
@@ -8,7 +8,7 @@ import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NbPaginationComponent } from './pagination.component';
 
-describe('NbPaginationComponent', () => {
+describe('Component: NbPagination', () => {
   let component: NbPaginationComponent;
   let fixture: ComponentFixture<NbPaginationComponent>;
 
