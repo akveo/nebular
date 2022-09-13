e8a4eed5851ac5418f63ebd9ee4f59c2482091bc
sashaqred <sashaqred@gmail.com>
2022-09-13 17:08:54 +0400

    feat: add dynamic multiple showcase

--- src/playground/with-layout/select/select-autocomplete-showcase.component.ts
+++ src/playground/with-layout/select/select-autocomplete-showcase.component.ts
@@ -12,6 +12,7 @@ import { Component } from '@angular/core';
 })
 export class SelectAutocompleteShowcaseComponent {
   withAutocomplete = true;
+  multiple = false;
   selectedItem = '2';
   filterValue = '';
 
