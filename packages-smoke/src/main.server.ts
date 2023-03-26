10ce86381e3b01b1b4a51c8185cd4c0b6fb8d5c0
denStrigo <denis2295@gmail.com>
2023-03-26 17:33:41 +0200

    feat: update package-smoke to Angular 15

--- packages-smoke/src/main.server.ts
+++ packages-smoke/src/main.server.ts
@@ -16,4 +16,4 @@ if (environment.production) {
 }
 
 export { AppServerModule } from './app/app.server.module';
-export { renderModule, renderModuleFactory } from '@angular/platform-server';
+export { renderModuleFactory } from '@angular/platform-server';
