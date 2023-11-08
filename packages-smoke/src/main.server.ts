2d1ddfcf5bc23547d205899adb73af276a656fd5
denStrigo <denis2295@gmail.com>
2023-11-08 13:32:56 +0100

    feat(angular): update angular to v16 in package smoke

--- packages-smoke/src/main.server.ts
+++ packages-smoke/src/main.server.ts
@@ -16,4 +16,4 @@ if (environment.production) {
 }
 
 export { AppServerModule } from './app/app.server.module';
-export { renderModuleFactory } from '@angular/platform-server';
+export { renderModule } from '@angular/platform-server';
