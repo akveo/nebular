6a2fad188fb2e5e34cc2f1a0f39074c8a210548c
denStrigo <denis2295@gmail.com>
2025-02-16 11:23:04 +0100

    chore: update angular to 19 versions packages-smoke

--- packages-smoke/src/app/app.component.ts
+++ packages-smoke/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'bs-root',
-  template: 'Nebular Works!!!',
+    selector: 'bs-root',
+    template: 'Nebular Works!!!',
+    standalone: false
 })
 export class AppComponent {
 }
