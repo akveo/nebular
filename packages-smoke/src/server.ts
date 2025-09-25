3b856c7d020ea7b6cbdcc56a38bf73922096d96e
eugene <eugeneath@ya.ru>
2025-09-25 21:33:56 +0400

    fix server.ts

--- packages-smoke/src/server.ts
+++ packages-smoke/src/server.ts
@@ -7,7 +7,7 @@ import {
 import express from 'express';
 import { join } from 'node:path';
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
+const browserDistFolder = join(process.cwd(), 'dist/packages-smoke/browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
@@ -41,9 +41,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
@@ -52,7 +50,7 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env.PORT || 4000;
   app.listen(port, (error) => {
     if (error) {
       throw error;
