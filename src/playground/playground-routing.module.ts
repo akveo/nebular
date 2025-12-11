0240d5e248dac9ea31c37b85dc546ac125ed45e3
Jan-Ole H. <82958415+hoermillerj@users.noreply.github.com>
2025-12-11 09:26:43 +0100

    Update playground-routing.module.ts

--- src/playground/playground-routing.module.ts
+++ src/playground/playground-routing.module.ts
@@ -16,14 +16,6 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./without-layout/without-layout.module').then((m) => m.WithoutLayoutModule),
   },
-  {
-    path: '',
-    loadChildren: () => import('./with-layout/with-layout.module').then(m => m.WithLayoutModule),
-  },
-  {
-    path: '',
-    loadChildren: () => import('./without-layout/without-layout.module').then(m => m.WithoutLayoutModule),
-  },
 ];
 
 @NgModule({
